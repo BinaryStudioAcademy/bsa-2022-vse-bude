7e1d6ddbbffffb9fb2539134b1b550ff91306ade
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2022-08-05 11:55:20 +0300

    Updated deploy script

--- scripts/README.md
+++ scripts/README.md
@@ -18,26 +18,15 @@ sudo apt update
 sudo apt install nginx
 ```
 
+Create folder `/home/ubuntu/env` with files `backend.env` and `frontend.env` (.env files)
+
 ## Deploy
 
-1. Clone repository to /home/ubuntu/vse-bude
-2. Copy nginx.conf from app folder to server
+1. Clone repository to `/home/ubuntu/vse-bude`
+2. Run deployment command
 
 ```
-cp nginx.conf /etc/nginx/nginx.conf
-```
-
-3. Start (or restart) nginx
-
-```
-sudo service nginx start
-sudo service nginx restart
-```
-
-4. Run deployment command
-
-```
-./scripts/deploy.sh
+sudo ./scripts/deploy.sh
 ```
 
 [Useful link](https://gist.github.com/ZaHuPro/2ecdb934a7362e979e3aa5a92b181153)
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-export DISABLE_ERD=true
 
+# configure nginx
+cp nginx.conf /etc/nginx/nginx.conf
+sudo systemctl start nginx
+sudo systemctl restart nginx
+sudo systemctl status nginx
+sudo nginx -t
+
+# copy .env paths
+cp ../env/backend.env ./packages/backend/.env
+cp ../env/frontend.env ./packages/frontend/.env
+
+# install and build
 npm install
 npm run install:shared
 npm run build:shared
@@ -8,8 +19,12 @@ npm run install:backend
 npm run install:frontend
 npm run build:backend
 npm run build:frontend
+
+# apply migrations
 cd ./packages/backend
 npm run db:migrate
 cd ../..
+
+# start production builds
 npm run production:start:backend
 npm run production:start:frontend
