28e48c4579b4ec01bd17325e2f17b49b0159077a
Vitalii Yarmus <vitalii.yarmys@binary-studio.com>
2022-08-05 12:06:29 +0300

    Fixed build scripts. Fixed production:start:backend script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # configure nginx
+echo "1. Configure nginx"
 cp nginx.conf /etc/nginx/nginx.conf
 sudo systemctl start nginx
 sudo systemctl restart nginx
@@ -8,10 +9,12 @@ sudo systemctl status nginx
 sudo nginx -t
 
 # copy .env paths
+echo "2. Copy .env files"
 cp ../env/backend.env ./packages/backend/.env
 cp ../env/frontend.env ./packages/frontend/.env
 
 # install and build
+echo "3. Install and build"
 npm install
 npm run install:shared
 npm run build:shared
@@ -21,10 +24,12 @@ npm run build:backend
 npm run build:frontend
 
 # apply migrations
+echo "4. Apply migrations"
 cd ./packages/backend
 npm run db:migrate
 cd ../..
 
 # start production builds
+echo "5. Run production build"
 npm run production:start:backend
 npm run production:start:frontend
