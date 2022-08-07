43f6b7a7cf4436e0b36ed0a35881d7a0ca63769e
Aleksandr Termenzhy <aleksandr.termenzhy@binary-studio.com>
2022-08-08 00:06:31 +0300

    Extracted validation schemas to shared package; fixed linter issues

--- packages/shared/src/common/enums/index.ts
+++ packages/shared/src/common/enums/index.ts
@@ -2,3 +2,4 @@ export * from './http';
 export * from './api';
 export * from './storage';
 export * from './validation';
+export * from './exception';
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
 export * from './common/enums';
 export * from './common/types';
 export * from './helpers';
+export * from './exceptions';
+export * from './validation-schemas';
