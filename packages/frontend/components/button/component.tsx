52661fe3fa044d78e2220400b3e6842c2955dfce
double-hope <nadia.prohorchuk@gmail.com>
2022-08-08 21:03:45 +0300

    separated styles with data-variant and data-size

--- packages/frontend/components/button/component.tsx
+++ packages/frontend/components/button/component.tsx
@@ -1,8 +1,8 @@
 import { Container } from '@primitives';
 import { StyledButton } from './styles';
 
-export const Button = (props) => (
-  <StyledButton form-variant={props.variant}>
+export const Button = ( props ) => (
+  <StyledButton data-variant={props.data_variant} data-size={props.data_size}>
     <Container>{props.text}</Container>
   </StyledButton>
 );
--- packages/frontend/components/button/styles.ts
+++ packages/frontend/components/button/styles.ts
@@ -1,23 +1,14 @@
 import { styled } from '@nextui-org/react';
 
 export const StyledButton = styled('button', {
-  padding: '13px 35px',
-  fontWeight: '$bold',
+
+  borderStyle: 'none',
   fontSize: '$base',
   lineHeight: '19px',
-  borderStyle: 'none',
-  borderRadius: '10px',
   fontFamily: '$sans',
+  fontWeight: '$bold',
 
-  'div': {
-    padding: '0',
-  },
-
-  '&:hover': {
-    cursor: 'pointer',
-  },
-
-  '&[form-variant="ordinary"]': {
+  '&[data-variant="ordinary"]': {
     background: '#DFDFDF',
     color: '$white',
     '&:hover': {
@@ -28,30 +19,27 @@ export const StyledButton = styled('button', {
     }
   },
 
-  '&[form-variant="empty"]': {
+  '&[data-variant="empty"]': {
+    border: '1px solid #DFDFDF',
     background: '$white',
     color: '#DFDFDF',
-    border: '1px solid #DFDFDF',
     '&:hover': {
-      color: '#F1B313',
       border: '1px solid #F1B313',
+      color: '#F1B313',
     },
     '&:active': {
-      color: '#2C4340',
       border: '1px solid #2C4340',
+      color: '#2C4340',
     }
   },
 
-  '&[form-variant="small"]': {
-    padding: '8px 20px',
-    background: '#DFDFDF',
-    color: '$white',
+  '&[data-size="big"]':{
+    borderRadius: '10px',
+    padding: '13px 35px',
+  },
+
+  '&[data-size="small"]':{
     borderRadius: '7px',
-    '&:hover': {
-      background: '#FFB800',
-    },
-    '&:active': {
-      background: '#F1B313',
-    }
-  }
+    padding: '8px 20px',
+  },
 });
