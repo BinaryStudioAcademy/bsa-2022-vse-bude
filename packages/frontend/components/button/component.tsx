9ec2b014244ab6124aa9ccb489485d934dd0436f
double-hope <nadia.prohorchuk@gmail.com>
2022-08-08 21:13:24 +0300

    delete Container from Button

--- packages/frontend/components/button/component.tsx
+++ packages/frontend/components/button/component.tsx
@@ -1,8 +1,7 @@
-import { Container } from '@primitives';
 import { StyledButton } from './styles';
 
 export const Button = ( props ) => (
   <StyledButton data-variant={props.data_variant} data-size={props.data_size}>
-    <Container>{props.text}</Container>
+    {props.text}
   </StyledButton>
 );
