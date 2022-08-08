cc03edb43d4f4c55ea941a5f703e031545921b2c
dannleed <59970198+dannleed@users.noreply.github.com>
2022-08-08 18:14:25 +0300

    VB-14: add color palette to shared

---
+++ packages/shared/src/common/enums/theme/color-palette.ts
@@ -0,0 +1,14 @@
+enum ColorPalette {
+  Green100 = '#778684',
+  Green200 = '#2C4340',
+  Yellow100 = '#F1B313',
+  Yellow200 = '#FFB800',
+  Gray100 = '#F5F5F5',
+  Gray200 = '#DFDFDF',
+  Gray300 = '#6A6C6D',
+  Red100 = '#E81414',
+  Black100 = '#2F2F2F',
+  White100 = '#FFFFFF',
+}
+
+export { ColorPalette };
