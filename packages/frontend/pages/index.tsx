221da4ef9797c3a2f706082bf1400bfdc8bad195
pm-yar <p.yar@PMsAirMYaroslav.lan>
2022-08-08 18:13:56 +0300

    #15: add i18n

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,3 +1,5 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 import { Button, Container, Loading, Card } from '@primitives';
 import { Http } from '@vse-bude/shared';
 import { Layout } from '@components';
@@ -7,24 +9,28 @@ import { shallowEqual } from 'react-redux';
 import { fetchRandomData, fetchRandomDataSSR, wrapper } from 'store';
 
 export const getServerSideProps = wrapper.getServerSideProps((store) => async (ctx) => {
+  const { locale } = ctx;
   const storage = new CookieStorage(ctx);
   const httpClient = new Http(process.env.NEXT_PUBLIC_API_ROUTE, storage);
 
   await store.dispatch(fetchRandomDataSSR(httpClient));
 
   return Promise.resolve({
-    props: {},
+    props: {
+      ...(await serverSideTranslations(locale, ['home'])),
+    },
   });
 });
 
 const IndexPage = () => {
   const { data, loading } = useTypedSelector((state) => state.randomData, shallowEqual);
   const dispatch = useAppDispatch();
+  const { t } = useTranslation('home');
 
   return (
     <Layout>
       <Container>
-        <h1>Random data (Server side by default)</h1>
+        <h1>{t('h1')}</h1>
         <Card>
           <Card.Body>
             <div style={{ wordBreak: 'break-all' }}>{JSON.stringify(data)}</div>
