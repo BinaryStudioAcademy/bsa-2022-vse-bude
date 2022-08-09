10495f7c1238818fd6cdd67bd5f88d5bc3e15561
natata7 <natata7@gmail.com>
2022-08-09 14:52:26 +0300

    VBM-32: change icon component

--- packages/mobile/src/common/enums/enums.ts
+++ packages/mobile/src/common/enums/enums.ts
@@ -1,3 +1,2 @@
 export { ENV, StorageKey, DataStatus } from './app/app';
 export { RootScreenName } from './navigation/navigation';
-export { IconName } from './ui/ui';
--- packages/mobile/src/components/icon/icon.tsx
+++ packages/mobile/src/components/icon/icon.tsx
@@ -1,29 +1,29 @@
 import React, { FC } from 'react';
-import { StyleProp, TouchableOpacity, ViewStyle } from 'react-native';
 import IconUI from 'react-native-vector-icons/Feather';
-import { IconName } from '~/common/enums/enums';
-import { Text } from '~/components/components';
-import { styles } from './styles';
 
 type Props = {
-  name: IconName;
   size: number;
   color?: string;
-  label?: string;
-  style?: StyleProp<ViewStyle>;
-  onPress?: () => void;
 };
 
-const Icon: FC<Props> = ({ name, size, style, color, label, onPress }) => (
-  <TouchableOpacity
-    activeOpacity={0.7}
-    disabled={!onPress}
-    style={[styles.container, style]}
-    onPress={onPress}
-  >
-    <IconUI name={name} size={size ? size : 20} color={color} />
-    {label && <Text style={[styles.label]}>{label}</Text>}
-  </TouchableOpacity>
+const HomeIcon: FC<Props> = (iconProps) => (
+  <IconUI name="home" {...iconProps} />
 );
 
-export { Icon };
+const StarIcon: FC<Props> = (iconProps) => (
+  <IconUI name="star" {...iconProps} />
+);
+
+const UserIcon: FC<Props> = (iconProps) => (
+  <IconUI name="user" {...iconProps} />
+);
+
+const SettingsIcon: FC<Props> = (iconProps) => (
+  <IconUI name="settings" {...iconProps} />
+);
+
+const ListIcon: FC<Props> = (iconProps) => (
+  <IconUI name="list" {...iconProps} />
+);
+
+export { HomeIcon, StarIcon, UserIcon, SettingsIcon, ListIcon };
