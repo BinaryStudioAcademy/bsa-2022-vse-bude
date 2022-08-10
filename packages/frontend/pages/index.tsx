759a390ffc22edd0603ca5b4453cb78ea31042a5
pm-yar <p.yar@PMsAirMYaroslav.lan>
2022-08-10 15:44:30 +0300

    #13: remove test data

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -7,7 +7,6 @@ import { CookieStorage } from '@helpers';
 import { useAppDispatch, useTypedSelector } from '@hooks';
 import { shallowEqual } from 'react-redux';
 import { fetchRandomData, fetchRandomDataSSR, wrapper } from 'store';
-import { StringCutter } from 'components/primitives/string-cutter';
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async (ctx) => {
@@ -60,11 +59,6 @@ const IndexPage = () => {
           >
             click me
           </Button>
-          <div style={{ width: '100%', color: 'red' }}>
-            <StringCutter>
-              sdfsdfsdfds sfdf sdfsdfdsf sdfsd fsd fsdf sdfsdf
-            </StringCutter>
-          </div>
         </div>
       </Container>
     </Layout>
