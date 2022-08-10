43a53cdba7c6a2389331beee745322df15c5618f
mishabenq <mixa200221@gmail.com>
2022-08-10 19:30:54 +0300

    VB-8: removed old config fields

--- packages/backend/prisma/seed/config/config.ts
+++ packages/backend/prisma/seed/config/config.ts
@@ -1,11 +1,5 @@
 export const USERS_NUMBER = 5;
 
-// export const BIDS_NUMBER = USERS_NUMBER * 2;
-// export const CHAT_NUMBER = USERS_NUMBER * 2;
-// export const CHAT_MEMBERS_NUMBER = (USERS_NUMBER * CHAT_NUMBER) / 2;
-// export const MESSAGES_NUMBER = CHAT_MEMBERS_NUMBER * 5;
-
-export const BIDS_NUMBER = USERS_NUMBER;
-export const CHAT_NUMBER = USERS_NUMBER;
-export const CHAT_MEMBERS_NUMBER = USERS_NUMBER;
-export const MESSAGES_NUMBER = USERS_NUMBER;
+export const BIDS_NUMBER = USERS_NUMBER * 2;
+export const CHAT_MEMBERS_NUMBER = USERS_NUMBER * 2;
+export const MESSAGES_NUMBER = CHAT_MEMBERS_NUMBER * 3;
