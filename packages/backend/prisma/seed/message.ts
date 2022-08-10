36e158b18057024505c645d0d21a006d62ce8f99
mishabenq <mixa200221@gmail.com>
2022-08-10 14:08:28 +0300

    VB-8: added message seed

---
+++ packages/backend/prisma/seed/message.ts
@@ -0,0 +1,24 @@
+import type { Message, PrismaClient, User } from '@prisma/client';
+import { fakeMessage } from './data/message';
+import { MESSAGES_NUMBER } from './config';
+
+export const seedMessage = async (
+  prismaClient: PrismaClient,
+  existingUsers: User[],
+) => {
+  const existingMessages = await prismaClient.message.findMany();
+  const howManyToCreate = MESSAGES_NUMBER - existingMessages.length;
+
+  if (howManyToCreate <= 0) {
+    return;
+  }
+
+  const existingChats = await prismaClient.chat.findMany();
+
+  const data: Message[] = await fakeMessage(
+    howManyToCreate,
+    existingUsers,
+    existingChats,
+  );
+  await prismaClient.message.createMany({ data: data });
+};
