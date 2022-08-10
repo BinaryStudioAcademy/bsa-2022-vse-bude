fd86847be3ceba070957530f80b10d69329ec627
mishabenq <mixa200221@gmail.com>
2022-08-10 19:29:09 +0300

    VB-8: added products random number feature

--- packages/backend/prisma/seed/controllers/product.ts
+++ packages/backend/prisma/seed/controllers/product.ts
@@ -1,7 +1,6 @@
 import type { PrismaClient, Product, User } from '@prisma/client';
-// import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import { fakeProducts } from '../data/Product';
-import { USERS_NUMBER } from '../config/config';
 
 export const seedProducts = async (
   prismaClient: PrismaClient,
@@ -9,9 +8,8 @@ export const seedProducts = async (
 ) => {
   const existingCategories = await prismaClient.category.findMany();
 
-  // const productsNumber =
-  //   faker.datatype.number({ min: 1, max: 3 }) * existingCategories.length;
-  const productsNumber = USERS_NUMBER;
+  const productsNumber =
+    faker.datatype.number({ min: 1, max: 3 }) * existingCategories.length;
 
   const existingProducts = await prismaClient.product.findMany();
   const howManyToCreate = productsNumber - existingProducts.length;
