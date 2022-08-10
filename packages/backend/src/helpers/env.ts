b3656db79cdeaabb84d585e51371f70a12a71a2e
Dmitriy Uvin <dimauuvin@gmail.com>
2022-08-10 03:39:57 +0300

    VB-4: add jwt library (+ @types)

--- packages/backend/src/helpers/env.ts
+++ packages/backend/src/helpers/env.ts
@@ -1,6 +1,9 @@
 type ProcessEnvKey = 'NODE_ENV' | 'PORT';
 
-type AuthEnvKeys = 'JWT_SECRET_KEY';
+type AuthEnvKeys =
+  | 'JWT_SECRET_KEY'
+  | 'JWT_ACCESS_TOKEN_EXPIRATION_MIN'
+  | 'REFRESH_TOKEN_EXPIRATION_MIN';
 
 type EnvKeys = ProcessEnvKey | AuthEnvKeys;
 
