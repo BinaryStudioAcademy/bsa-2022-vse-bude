a63a966b91f3e9b9670770b084b47e873c8758b9
Val_M 9 <v.mischenko90@gmail.com>
2022-08-10 21:11:46 +0300

    VBM-47: add keyboardAvoidingView with keyboardHeight hook

---
+++ packages/mobile/src/components/keyboard-avoiding/keyboard-avoiding.tsx
@@ -0,0 +1,24 @@
+import React, { FC, ReactElement } from 'react';
+import { View } from '~/components/components';
+import { useKeyboardHeight } from '~/hooks/hooks';
+
+type KeyboardAvoidingProps = {
+  children: ReactElement;
+  style: Record<string, unknown>;
+};
+
+const KeyboardAvoiding: FC<KeyboardAvoidingProps> = ({ children, style }): ReactElement => {
+  const keyboardHeight = useKeyboardHeight();
+
+  //TODO: use animation for slide behavior
+
+  return (
+    <View
+      style={[{ paddingBottom: keyboardHeight / 4 }, style]}
+    >
+      {children}
+    </View>
+  );
+};
+
+export { KeyboardAvoiding };
