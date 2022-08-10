5b845e031b3b8cf874cba568979e1137bdb00942
mishabenq <mixa200221@gmail.com>
2022-08-10 19:30:04 +0300

    VB-8: fixed chat seed foreign key error

--- packages/backend/prisma/seed/controllers/chat.ts
+++ packages/backend/prisma/seed/controllers/chat.ts
@@ -1,5 +1,4 @@
 import type { Chat, PrismaClient, Product } from '@prisma/client';
-import { CHAT_NUMBER } from '../config/config';
 import { fakeChat } from '../data/chat';
 
 export const seedChat = async (
@@ -7,11 +6,13 @@ export const seedChat = async (
   existingProducts: Product[],
 ) => {
   const existingChats = await prismaClient.chat.findMany();
-  const howManyToCreate = CHAT_NUMBER - existingChats.length;
+  const howManyToCreate = existingProducts.length - existingChats.length;
 
   if (howManyToCreate <= 0) {
     return;
   }
+
+  await prismaClient.chat.deleteMany({});
 
   const data: Chat[] = await fakeChat(howManyToCreate, existingProducts);
   await prismaClient.chat.createMany({ data: data });
