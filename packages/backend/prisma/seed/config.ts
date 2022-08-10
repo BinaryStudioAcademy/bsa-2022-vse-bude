e4738cedf46cfb7a77315c661894f399c9113052
mishabenq <mixa200221@gmail.com>
2022-08-10 14:12:41 +0300

    VB-8: some configuration

--- packages/backend/prisma/seed/config.ts
+++ packages/backend/prisma/seed/config.ts
@@ -4,7 +4,9 @@ export const CATEGORY_NUMBER = 10;
 // export const BIDS_NUMBER = USERS_NUMBER * 2;
 // export const CHAT_NUMBER = USERS_NUMBER * 2;
 // export const CHAT_MEMBERS_NUMBER = (USERS_NUMBER * CHAT_NUMBER) / 2;
+// export const MESSAGES_NUMBER = CHAT_MEMBERS_NUMBER * 5;
 
 export const BIDS_NUMBER = USERS_NUMBER;
 export const CHAT_NUMBER = USERS_NUMBER;
 export const CHAT_MEMBERS_NUMBER = USERS_NUMBER;
+export const MESSAGES_NUMBER = USERS_NUMBER;
--- packages/backend/prisma/seed/index.ts
+++ packages/backend/prisma/seed/index.ts
@@ -4,10 +4,11 @@ import { seedAddress } from './address';
 import { seedBids } from './bid';
 import { seedCategory } from './category';
 import { seedProducts } from './product';
-// import { seedSocialMedia } from './socialMedia';
+import { seedSocialMedia } from './socialMedia';
 import { seedUsers } from './user';
 import { seedUserSettings } from './userSettings';
 import { seedChatMember } from './chatMember';
+import { seedMessage } from './message';
 
 const prismaClient = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
@@ -15,14 +16,15 @@ const prismaClient = new PrismaClient({
 
 (async () => {
   //clear
-  // await prismaClient.address.deleteMany({});
-  // await prismaClient.userSettings.deleteMany({});
-  // await prismaClient.category.deleteMany({});
-  // await prismaClient.bid.deleteMany({});
-  // await prismaClient.chatMember.deleteMany({});
-  // await prismaClient.chat.deleteMany({});
-  // await prismaClient.product.deleteMany({});
-  // await prismaClient.user.deleteMany({});
+  await prismaClient.address.deleteMany({});
+  await prismaClient.userSettings.deleteMany({});
+  await prismaClient.category.deleteMany({});
+  await prismaClient.bid.deleteMany({});
+  await prismaClient.chatMember.deleteMany({});
+  await prismaClient.message.deleteMany({});
+  await prismaClient.chat.deleteMany({});
+  await prismaClient.product.deleteMany({});
+  await prismaClient.user.deleteMany({});
 
   await seedUsers(prismaClient);
 
@@ -31,12 +33,13 @@ const prismaClient = new PrismaClient({
 
   await seedUserSettings(prismaClient, existingUsers);
   await seedAddress(prismaClient, existingUsers);
-  // await seedSocialMedia(prismaClient, existingUsers);
   await seedCategory(prismaClient);
   await seedProducts(prismaClient, existingUsers);
+  await seedSocialMedia(prismaClient, existingUsers);
   await seedBids(prismaClient, existingUsers);
   await seedChat(prismaClient);
   await seedChatMember(prismaClient, existingUsers);
+  await seedMessage(prismaClient, existingUsers);
 })()
   .then(() => prismaClient.$disconnect())
   .catch(async (e) => {
