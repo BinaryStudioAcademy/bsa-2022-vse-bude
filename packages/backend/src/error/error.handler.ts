d6e27f94440980364de54a7a5820f0c3f7c52304
Dmitriy Uvin <dimauuvin@gmail.com>
2022-08-10 22:08:07 +0300

    VB-4: add error handler

---
+++ packages/backend/src/error/error.handler.ts
@@ -0,0 +1,23 @@
+import type { Request, Errback, Response, NextFunction } from 'express';
+import { HttpError, HttpStatusCode } from '@vse-bude/shared';
+
+export const errorHandler = (
+  err: Errback,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!err) {
+    next();
+  }
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({
+    error: 'interna;',
+  });
+};
