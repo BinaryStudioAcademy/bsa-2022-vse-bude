d08e5abc7cebf0bbde5bdeb7abb8fa4f59e5835a
Val-M9 <71255723+Val-M9@users.noreply.github.com>
2022-08-11 16:37:04 +0300

    Update enums.ts

--- packages/mobile/src/common/enums/enums.ts
+++ packages/mobile/src/common/enums/enums.ts
@@ -1,6 +1,5 @@
 export { ENV, StorageKey, DataStatus } from './app/app';
-export { FontFamily, FontSize } from './fonts/fonts';
 export { RootScreenName, MainScreenName } from './navigation/navigation';
 export { NotificationType } from './notification/notification.enum';
 export { ButtonType, ButtonAppearance } from './ui/ui';
-
+export { FontFamily, FontSize } from './fonts/fonts';
