6dae4c1e7b234e54101363c52011a49c41728d28
double-hope <nadia.prohorchuk@gmail.com>
2022-08-12 00:04:06 +0300

    VBM-22: add popover

---
+++ packages/frontend/components/primitives/popover/target/component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface CustomComponentProps extends React.ComponentPropsWithoutRef<'div'> {
+  onClick(): void;
+}
+
+const Target = React.forwardRef<HTMLDivElement, CustomComponentProps>(
+  (props, ref) => (
+    <div>
+      <button
+      ref={ref}
+      onClick={props.onClick}
+      style={{ color: 'black', width: '200px' }}
+    >
+      {props.children}
+    </button>
+      
+    </div>
+  ));
+
+Target.displayName = 'Target';
+export default Target;
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import { Button, Container } from '@primitives';
@@ -7,6 +8,9 @@ import { CookieStorage } from '@helpers';
 import { useAppDispatch, useTypedSelector } from '@hooks';
 import { shallowEqual } from 'react-redux';
 import { fetchRandomData, fetchRandomDataSSR, wrapper } from 'store';
+import { useRef } from 'react';
+import MyPopover from 'components/primitives/popover/component';
+import Target from 'components/primitives/popover/target/component';
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async (ctx) => {
@@ -28,6 +32,11 @@ const IndexPage = () => {
   const { data } = useTypedSelector((state) => state.randomData, shallowEqual);
   const dispatch = useAppDispatch();
   const { t } = useTranslation('home');
+  const popoverRef = useRef(null);
+
+  const handleClick = () => {
+    popoverRef.current.setVisible(true);
+  };
 
   return (
     <Layout>
@@ -35,6 +44,25 @@ const IndexPage = () => {
         <h1>{t('h1')}</h1>
         <div>
           <div style={{ wordBreak: 'break-all' }}>{JSON.stringify(data)}</div>
+
+          <MyPopover
+            ref={popoverRef}
+            body={
+              <Target onClick={handleClick}>
+                hey from a custom target component
+              </Target>
+            }
+          >
+            <ul>
+              <li>Personal Info</li>
+              <li>My List</li>
+              <li>Settings</li>
+              <li>Messages</li>
+              <li>Support</li>
+              <li>Sign Out</li>
+            </ul>
+          </MyPopover>
+
           <Button
             variant="outlined"
             onClick={() => dispatch(fetchRandomData())}
