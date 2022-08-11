5a5c81695ea2c379bd0dd57b9b6f0bfc397b62e7
mishabenq <mixa200221@gmail.com>
2022-08-11 20:18:56 +0300

    VB-8: finished seed update function

--- packages/backend/prisma/seed/update.ts
+++ packages/backend/prisma/seed/update.ts
@@ -1,8 +1,26 @@
+import { updateUserSettings } from './controllers/update/userSettings';
+import { updateSocialMedia } from './controllers/update/socialMedia';
+import { updateProduct } from './controllers/update/product';
+import { updateMessage } from './controllers/update/message';
+import { updateChatMember } from './controllers/update/chatMember';
+import { updateChat } from './controllers/update/chat';
+import { updateCategory } from './controllers/update/category';
+import { updateBid } from './controllers/update/bid';
 import { prismaClient } from './config/prismaClient';
+import { updateAddress } from './controllers/update/address';
 import { updateUsers } from './controllers/update/user';
 
 (async () => {
+  await updateAddress(prismaClient);
+  await updateBid(prismaClient);
+  await updateCategory(prismaClient);
+  await updateChat(prismaClient);
+  await updateChatMember(prismaClient);
+  await updateMessage(prismaClient);
+  await updateProduct(prismaClient);
+  await updateSocialMedia(prismaClient);
   await updateUsers(prismaClient);
+  await updateUserSettings(prismaClient);
 })()
   .then(() => prismaClient.$disconnect())
   .catch(async (e) => {
