dd26a5c470f6a1998adcecc71fece826ff961702
Val_M 9 <v.mischenko90@gmail.com>
2022-08-12 13:51:32 +0300

    delete redundant props, made default fontStyles

--- packages/mobile/src/components/text/text.tsx
+++ packages/mobile/src/components/text/text.tsx
@@ -1,23 +1,19 @@
 import React, { FC, ReactElement } from 'react';
 import { Text as NativeText, TextProps as RNTextProps } from 'react-native';
-import { FontFamily, FontSize } from '~/common/enums/enums';
+import { styles } from './styles';
 
 type TextProps = RNTextProps & {
-  fontVariant?: FontFamily;
-  size?: FontSize;
   children: string;
 };
 
 const Text: FC<TextProps> = ({
-  fontVariant = FontFamily.RALEWAY_REGULAR,
-  size = 16,
   style,
   children,
   ...restProps
 }): ReactElement => {
   return (
     <NativeText
-      style={[{ fontFamily: fontVariant, fontSize: size }, style]}
+      style={[styles.default, style]}
       {...restProps}
     >
       {children}
