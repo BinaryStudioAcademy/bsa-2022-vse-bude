625a661a6f2d37b9b6b2a8c46b31aed23013955f
mishabenq <mixa200221@gmail.com>
2022-08-12 11:29:10 +0300

    VB-8: updated JSON stringify to look data readable

--- packages/backend/prisma/seed/helpers/wtireFile.ts
+++ packages/backend/prisma/seed/helpers/wtireFile.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export const writeFile = (fileName, data) => {
   const path = `./prisma/seed/mockData/${fileName}.json`;
-  const jsonContent = JSON.stringify(data);
+  const jsonContent = JSON.stringify(data, null, 4);
   fs.writeFile(path, jsonContent, 'utf8', (err) => {
     if (err) {
       return console.log(err);
