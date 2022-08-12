9dd852652fbf0ab48e7d191baf7bf8d3b180c263
double-hope <nadia.prohorchuk@gmail.com>
2022-08-12 21:38:10 +0300

    VBM-59: add route /health

---
+++ packages/backend/src/api/routes/health.ts
@@ -0,0 +1,24 @@
+import type { ApiRoutes } from '@vse-bude/shared';
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { RedisStorageService } from 'services/redis-storage';
+
+export const initHealthRoutes = (path: ApiRoutes) => {
+  const router = Router();
+  let status = 200;
+
+  const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+
+  try {
+    prisma.$connect();
+    new RedisStorageService();
+  } catch (e) {
+    status = 503;
+  }
+
+  return router.get(path, (req, res) => {
+    res.send(`status: ${status}`);
+  });
+};
