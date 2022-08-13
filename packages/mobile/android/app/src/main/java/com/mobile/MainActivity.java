7d0b870511766ed5edf9e89e2b08fa73ca77c6de
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-08-13 11:33:58 +0300

    VBM-16: splash screen bg change

--- packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/mobile/MainActivity.java
@@ -4,7 +4,6 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import org.devio.rn.splashscreen.SplashScreen;
-import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
