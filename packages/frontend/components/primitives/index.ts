510ef82aa643e6373d614c5a749a8887a1c6cc2c
Dan Strelets <59970198+dannleed@users.noreply.github.com>
2022-08-15 01:13:32 +0300

    Update index.ts

--- packages/frontend/components/primitives/index.ts
+++ packages/frontend/components/primitives/index.ts
@@ -3,6 +3,7 @@ export { Tooltip } from './tooltip';
 export { Button } from './button';
 export { Checkbox } from './checkbox';
 export { InternalLink, Anchor } from './link';
+export { StringCutter } from './string-cutter';
 export { Avatar } from './avatar';
 export { Dropdown } from './menu-dropdown';
 export { Icon } from './icon';
