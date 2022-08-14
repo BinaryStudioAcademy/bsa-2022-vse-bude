3df162ec7424f878a648ab04c707d1df69afe312
Val_M 9 <v.mischenko90@gmail.com>
2022-08-14 12:54:26 +0300

    add types for localization keys

--- packages/mobile/src/localization/i18n.ts
+++ packages/mobile/src/localization/i18n.ts
@@ -2,6 +2,11 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { ua, en } from './languages/languages';
 
+const resources = {
+  en: { translation: en },
+  ua: { translation: ua },
+};
+
 const i18 = () => {
   i18n.use(initReactI18next).init({
     compatibilityJSON: 'v3',
@@ -11,11 +16,8 @@ const i18 = () => {
     interpolation: {
       escapeValue: false,
     },
-    resources: {
-      en: { translation: en },
-      ua: { translation: ua },
-    },
+    resources: resources,
   });
 };
 
-export { i18 };
+export { i18, resources };
