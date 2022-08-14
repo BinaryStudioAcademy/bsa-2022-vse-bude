60c5259cef76d65ceb956adcd9db53665533127e
Dmitriy Uvin <dimauuvin@gmail.com>
2022-08-14 16:05:06 +0300

    VB-27: add validation for sign-up

--- packages/frontend/pages/sign-up/index.tsx
+++ packages/frontend/pages/sign-up/index.tsx
@@ -1,83 +1,18 @@
-import { Button } from '@primitives';
-import { Input, PasswordInput } from 'components/primitives/input';
-import type { SubmitHandler } from 'react-hook-form';
-import { useForm } from 'react-hook-form';
 import { InternalLink } from 'components/primitives/link';
-import type { UserSignUpDto } from '@vse-bude/shared';
 import { AuthLayout } from '../../components/authLayout/component';
 import {
   contentWrapper,
   formWrapper,
-  form,
-  inputWrapper,
   linkText,
 } from '../../components/auth/layout/styles';
+import { SignUpForm } from '../../components/auth/sign-up/component';
 
-export default function LoginPage() {
-  const { register, handleSubmit } = useForm<UserSignUpDto>({});
-  const onSubmit: SubmitHandler<UserSignUpDto> = (data) => console.log(data);
-
+export default function SignUpPage() {
   return (
     <AuthLayout>
       <div css={contentWrapper}>
         <div css={formWrapper}>
-          <form onSubmit={handleSubmit(onSubmit)} css={form}>
-            <div css={inputWrapper}>
-              <Input
-                {...register('firstName')}
-                label="First Name"
-                variant="primary"
-                type="text"
-                name="firstName"
-              />
-            </div>
-            <div css={inputWrapper}>
-              <Input
-                {...register('lastName')}
-                label="Last Name"
-                variant="primary"
-                type="text"
-                name="lastName"
-              />
-            </div>
-            <div css={inputWrapper}>
-              <Input
-                {...register('phone')}
-                label="Phone Number"
-                variant="primary"
-                type="text"
-                name="phone"
-              />
-            </div>
-            <div css={inputWrapper}>
-              <Input
-                {...register('email')}
-                label="Email"
-                variant="primary"
-                type="email"
-                name="email"
-              />
-            </div>
-            <div css={inputWrapper}>
-              <PasswordInput
-                {...register('password')}
-                label="Password"
-                variant="primary"
-                name="password"
-              />
-            </div>
-            <div css={inputWrapper}>
-              <PasswordInput
-                {...register('password')}
-                label="Repeat password"
-                variant="primary"
-                name="repeatPassword"
-              />
-            </div>
-            <Button type="submit" width={'100%'}>
-              Create Account
-            </Button>
-          </form>
+          <SignUpForm />
         </div>
         <div css={linkText}>
           I have an account!{' '}
