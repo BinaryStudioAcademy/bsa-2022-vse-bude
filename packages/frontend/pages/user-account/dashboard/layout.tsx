2cad11cb26b540889f820c266fb344cea5ac8f90
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-08-15 09:54:15 +0300

    vse-bude-task#38: deleted test subpage

--- packages/frontend/pages/user-account/dashboard/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react';
-import { Row } from '@primitives';
-import type { NestedLayoutProps } from './types';
-
-export const NestedLayout: FC<NestedLayoutProps> = ({ children }) => (
-  <Row>{children}</Row>
-);
