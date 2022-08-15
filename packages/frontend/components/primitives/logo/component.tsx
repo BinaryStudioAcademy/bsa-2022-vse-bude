9165ea3b3ec6ee3e0557ac073b92458ef2f04227
mishabenq <mixa200221@gmail.com>
2022-08-15 14:13:42 +0300

    VB-57: fixed logo styles

--- packages/frontend/components/primitives/logo/component.tsx
+++ packages/frontend/components/primitives/logo/component.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import logo from '../../../public/images/icons/logo.svg';
+import * as styles from './styles';
 
 export const Logo = () => (
-  <Image src={logo.src} width={128} height={32} alt="logo" />
+  <div css={styles.logo}>
+    <Image src={logo.src} layout="fill" alt="logo" />
+  </div>
 );
--- packages/frontend/components/primitives/logo/styles.ts
+++ packages/frontend/components/primitives/logo/styles.ts
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import type { Theme } from 'theme';
 
 export const logo = (theme: Theme) => css`
+  position: relative;
   display: block;
   width: ${theme.widths.logo};
   height: ${theme.heights.logo};
