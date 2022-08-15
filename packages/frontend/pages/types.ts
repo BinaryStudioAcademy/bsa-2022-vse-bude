0b9c096f771ea23e4d89fe83518773c54f4fe3a6
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-08-15 20:13:53 +0300

    vse-bude-task#38: changed component type t5

--- packages/frontend/pages/types.ts
+++ packages/frontend/pages/types.ts
@@ -2,10 +2,12 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
-export type NextPageWithLayout = NextPage & {
+type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
 export type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
+export default NextPageWithLayout;
