7874922580ba36a57f31905c2ef4881580849db4
mishabenq <mixa200221@gmail.com>
2022-08-15 14:09:43 +0300

    VB-57: returned next/image

--- packages/frontend/components/primitives/logo/component.tsx
+++ packages/frontend/components/primitives/logo/component.tsx
@@ -1,3 +1,6 @@
-import * as styles from './styles';
+import Image from 'next/image';
+import logo from '../../../public/images/icons/logo.svg';
 
-export const Logo = () => <span css={styles.logo}></span>;
+export const Logo = () => (
+  <Image src={logo.src} width={128} height={32} alt="logo" />
+);
