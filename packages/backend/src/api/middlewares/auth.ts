79c67ecd503d27758613db373a0956a011d6506a
yevhenii-moroziuk <kemd99@gmail.com>
2022-08-15 16:10:28 +0300

    Move auth parts to corresponding folders

--- packages/backend/src/api/middlewares/auth.ts
+++ packages/backend/src/api/middlewares/auth.ts
@@ -1,18 +1,16 @@
 import type { NextFunction, Request, Response } from 'express';
-import { HttpStatusCode } from '@vse-bude/shared';
+import { HttpHeader, HttpStatusCode } from '@vse-bude/shared';
 import { verify as jwtVerify } from 'jsonwebtoken';
-import { getEnv } from '@helpers';
+import { getEnv, getBearerValue } from '@helpers';
 import type { UserSessionJwtPayload } from 'jsonwebtoken';
 import { UNAUTHORIZED } from '@errors';
-import { AUTH_HEADER_NAME } from '../constants';
-import { getBearerValue } from '../functions';
 
 export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const authHeader = req.header(AUTH_HEADER_NAME);
+  const authHeader = req.header(HttpHeader.AUTHORIZATION);
   if (!authHeader) {
     return res.status(HttpStatusCode.UNAUTHORIZED).json({
       error: UNAUTHORIZED,
