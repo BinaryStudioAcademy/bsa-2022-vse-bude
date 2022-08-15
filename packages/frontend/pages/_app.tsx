ba3de65af9bf0d158ca25931b5c52539f2deb3fd
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-08-15 20:29:20 +0300

    vse-bude-task#38: changed types dir t6

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { appWithTranslation } from 'next-i18next';
 import { wrapper } from 'store';
 import { ThemeProvider } from '@providers';
-import type { AppPropsWithLayout } from './types';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
