e8f44ef0921ceba766a3f7029ad58279b722fc12
double-hope <nadia.prohorchuk@gmail.com>
2022-08-16 18:42:08 +0300

    small fixes

--- packages/frontend/components/primitives/popover/component.tsx
+++ packages/frontend/components/primitives/popover/component.tsx
@@ -1,25 +1,32 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { resetButton } from 'theme';
-import { useOutsideClick } from 'hooks/popover';
+import { useOutsideClick } from '@hooks';
 import * as styles from './styles';
 import type { PopoverProps } from './types';
 
 export const Popover = ({ trigger, children }: PopoverProps) => {
   const [isVisible, setIsVisible] = useState(false);
   const bodyRef = useRef<HTMLDivElement>();
-  const triggerWrapperRef = useRef<HTMLButtonElement>();
+
+  const handleClickOutside = useCallback(() => setIsVisible(false), []);
+  const triggerWrapperRef = useOutsideClick(handleClickOutside);
+
+  const getTriggerRectParams = useCallback(() =>
+    triggerWrapperRef.current.getBoundingClientRect(), [triggerWrapperRef]);
+
+  const getBodyRectParams = () => bodyRef.current.getBoundingClientRect();
 
   const calcBodyCoords = useCallback(() => {
     const triggerRectParams = getTriggerRectParams();
-
     const bodyRectParams = getBodyRectParams();
+
     const bodyTop = triggerRectParams.bottom;
     const bodyRight =
       triggerRectParams.left + triggerRectParams.width - bodyRectParams.width;
 
     return [bodyTop, bodyRight];
-  }, []);
+  }, [getTriggerRectParams]);
 
   useEffect(() => {
     if (bodyRef.current) {
@@ -29,20 +36,9 @@ export const Popover = ({ trigger, children }: PopoverProps) => {
     }
   }, [isVisible, calcBodyCoords]);
 
-  const getTriggerRectParams = () =>
-    triggerWrapperRef.current.getBoundingClientRect();
-
-  const getBodyRectParams = () => bodyRef.current.getBoundingClientRect();
-
   const handleMouseClick = () => {
     setIsVisible(true);
   };
-
-  const onClose = () => {
-    setIsVisible(false);
-  };
-
-  useOutsideClick(triggerWrapperRef, onClose, isVisible);
 
   const renderPortalBody = () => (
     <div ref={bodyRef} css={styles.popover}>
