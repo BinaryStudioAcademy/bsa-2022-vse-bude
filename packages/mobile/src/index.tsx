d4becfa9ab7482afae54a833a6e43965ffbfb031
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-08-16 14:11:29 +0300

    VBM-16: splash screen

--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -5,12 +5,16 @@ import 'fast-text-encoding';
 import { Navigation } from '~/navigation/navigation';
 import { store } from '~/store/store';
 import { NavigationDarkTheme, NavigationTheme } from '~/config/config';
-import { useColorScheme } from '~/hooks/hooks';
+import { useColorScheme, useEffect } from '~/hooks/hooks';
+import RNBootSplash from 'react-native-bootsplash';
 import { i18 } from './localization/localization';
 
 i18();
 
 const App: FC = () => {
+  useEffect(() =>{
+    RNBootSplash.hide({ fade: true });
+  },[]);
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? NavigationDarkTheme : NavigationTheme;
 
