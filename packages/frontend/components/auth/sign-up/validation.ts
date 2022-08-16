374117d6aaff0cedc4f54758e5a022ce7072564e
Dmitriy Uvin <dimauuvin@gmail.com>
2022-08-16 19:22:20 +0300

    VB-27: fix code style

--- packages/frontend/components/auth/sign-up/validation.ts
+++ packages/frontend/components/auth/sign-up/validation.ts
@@ -8,13 +8,11 @@ import type { UserSignUpDto } from '@vse-bude/shared';
 export const signUpSchema = Joi.object<UserSignUpDto>({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
-  phone: Joi.string().required()
-.pattern(phoneMask),
+  phone: Joi.string().required().pattern(phoneMask),
   email: Joi.string()
     .trim()
     .email({ tlds: { allow: false } })
     .required(),
-  password: Joi.string().min(minPasswordLength)
-.required(),
+  password: Joi.string().min(minPasswordLength).required(),
   repeatPassword: Joi.any().valid(Joi.ref('password')),
 });
