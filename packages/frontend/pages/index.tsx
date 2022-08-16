ca921d21fd834a5aee574971b868e21a3e4550b3
double-hope <nadia.prohorchuk@gmail.com>
2022-08-16 16:12:07 +0300

    merge main

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -38,11 +38,6 @@ const IndexPage = () => {
   const { data } = useTypedSelector((state) => state.randomData, shallowEqual);
   const dispatch = useAppDispatch();
   const { t } = useTranslation('home');
-  const popoverRef = useRef(null);
-
-  const handleClick = () => {
-    popoverRef.current.setVisible(true);
-  };
 
   const [isFavorite, setIsFavorite] = useState(false);
 
