58402e0a29c9f8fa49f2e415f597b82a9a84c04a
pm-yar <p.yar@PMsAirMYaroslav.lan>
2022-08-16 12:31:11 +0300

    VB-77 remove logic from app component

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,31 +1,13 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { wrapper } from 'store';
-import { ThemeProvider } from '@providers';
+import { AuthProvider, ThemeProvider } from '@providers';
 
-import { useEffect } from 'react';
-import { auth as authHelper } from '@helpers';
-import { useAppDispatch, useTypedSelector } from '@hooks';
-import { getCurrentUser } from 'store/profile';
-
-const App = ({ Component, pageProps }: AppProps) => {
-  const { user } = useTypedSelector((state) => state.profile);
-
-  const dispatch = useAppDispatch();
-
-  const hasToken = !!authHelper.getAccessToken();
-  const hasUser = !!user;
-
-  useEffect(() => {
-    if (!hasUser && hasToken) {
-      dispatch(getCurrentUser());
-    }
-  }, [dispatch, hasUser, hasToken]);
-
-  return (
-    <ThemeProvider>
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider>
+    <AuthProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
-  );
-};
+    </AuthProvider>
+  </ThemeProvider>
+);
 export default wrapper.withRedux(appWithTranslation(App));
