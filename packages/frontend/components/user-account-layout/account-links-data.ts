65c76e40d358fe593122862b739397435d6545a9
dannleed <59970198+dannleed@users.noreply.github.com>
2022-08-16 23:00:17 +0300

    VB-64: swap FontAwesomeIcon component with web fonts + css icon element

--- packages/frontend/components/user-account-layout/account-links-data.ts
+++ packages/frontend/components/user-account-layout/account-links-data.ts
@@ -1,34 +1,34 @@
-import { IconName, IconNameToSvgIcon, Routes, AccountRoutes } from '@enums';
+import { IconName, IconNameToClassName, Routes, AccountRoutes } from '@enums';
 import type { LinkData } from './types';
 
 export const linksData: LinkData[] = [
   {
-    iconPath: IconNameToSvgIcon[IconName.USER],
+    iconPath: IconNameToClassName[IconName.USER],
     label: 'PERSONAL_INFO',
     path: Routes.USER_ACCOUNT + AccountRoutes.ACCOUNT_PERSONAL,
   },
   {
-    iconPath: IconNameToSvgIcon[IconName.LIST],
+    iconPath: IconNameToClassName[IconName.LIST],
     label: 'MY_LIST',
     path: Routes.USER_ACCOUNT + AccountRoutes.ACCOUNT_LIST,
   },
   {
-    iconPath: IconNameToSvgIcon[IconName.SETTINGS],
+    iconPath: IconNameToClassName[IconName.SETTINGS],
     label: 'SETTINGS',
     path: Routes.USER_ACCOUNT + AccountRoutes.ACCOUNT_SETTINGS,
   },
   {
-    iconPath: IconNameToSvgIcon[IconName.MESSAGE],
+    iconPath: IconNameToClassName[IconName.MESSAGE],
     label: 'MESSAGES',
     path: Routes.USER_ACCOUNT + AccountRoutes.ACCOUNT_MESSAGES,
   },
   {
-    iconPath: IconNameToSvgIcon[IconName.SUPPORT],
+    iconPath: IconNameToClassName[IconName.SUPPORT],
     label: 'SUPPORT',
     path: Routes.USER_ACCOUNT + AccountRoutes.ACCOUNT_SUPPORT,
   },
   {
-    iconPath: IconNameToSvgIcon[IconName.SIGN_OUT],
+    iconPath: IconNameToClassName[IconName.SIGN_OUT],
     label: 'SIGN_OUT',
     path: Routes.DEFAULT,
   },
