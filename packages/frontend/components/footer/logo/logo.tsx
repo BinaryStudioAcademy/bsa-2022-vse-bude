979498dd7e6e83113da0bb52ba71f1ca3d6c1553
YevheniiBieliaiev <84641178+YevheniiBieliaiev@users.noreply.github.com>
2022-08-17 10:55:45 +0300

    Feature/footer (#83)
    * vse-bude-task#52: changed names added fontawesome styles
    
    * vse-bude-task#52: changed names added fontawesome styles t2
    
    * vse-bude-task#52: changed names added fontawesome styles t3
    
    * vse-bude-task#52: deleted props
    
    * vse-bude-task#52: deleted props t2
    
    * vse-bude-task#52: deleted props t3

--- packages/frontend/components/footer/logo/logo.tsx
+++ packages/frontend/components/footer/logo/logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import footerlogo from '../../../public/images/footer/footer-logo.svg';
 import * as styles from './styles';
 
-export const FooterLogo = (): JSX.Element => (
+export const Logo = (): JSX.Element => (
   <div css={styles.footerLogo}>
     <Image src={footerlogo} width={145} height={36} layout="fixed" alt="logo" />
   </div>
