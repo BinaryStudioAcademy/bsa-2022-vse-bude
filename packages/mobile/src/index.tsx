7774c7798211b705362f24053cc775c4bc0de0ac
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-08-18 16:34:13 +0300

    VBM-16: logo size changed

--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -2,25 +2,27 @@ import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider as StoreProvider } from 'react-redux';
 import 'fast-text-encoding';
+import RNBootSplash from 'react-native-bootsplash';
 import { Navigation } from '~/navigation/navigation';
 import { store } from '~/store/store';
 import { NavigationDarkTheme, NavigationTheme } from '~/config/config';
-import { useColorScheme, useEffect } from '~/hooks/hooks';
-import RNBootSplash from 'react-native-bootsplash';
+import { useColorScheme } from '~/hooks/hooks';
 import { i18 } from './localization/localization';
 
 i18();
 
 const App: FC = () => {
-  useEffect(() => {
-    RNBootSplash.hide({ fade: true });
-  }, []);
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? NavigationDarkTheme : NavigationTheme;
 
   return (
     <StoreProvider store={store}>
-      <NavigationContainer theme={theme}>
+      <NavigationContainer
+        theme={theme}
+        onReady={() => {
+          RNBootSplash.hide({ fade: true });
+        }}
+      >
         <Navigation />
       </NavigationContainer>
     </StoreProvider>
