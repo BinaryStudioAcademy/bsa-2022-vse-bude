e01154b9dca553965f4b24db89d62c199546b2d7
double-hope <nadia.prohorchuk@gmail.com>
2022-08-18 14:03:50 +0300

    VB-79: add useCheckAuth

--- packages/frontend/helpers/useCheckAuth.ts
+++ packages/frontend/helpers/useCheckAuth.ts
@@ -1,23 +1,23 @@
-import { auth } from "@helpers";
-import { useAppDispatch, useTypedSelector } from "@hooks";
-import { useEffect, useState } from "react";
-import { getCurrentUser } from "store/profile";
+import { auth } from '@helpers';
+import { useAppDispatch, useTypedSelector } from '@hooks';
+import { useEffect, useState } from 'react';
+import { getCurrentUser } from 'store/profile';
 
 export const useCheckAuth = () => {
-    const { user } = useTypedSelector((state) => state.profile);
-    const [isAuth, setIsAuth] = useState(false);
+  const { user } = useTypedSelector((state) => state.profile);
+  const [isAuth, setIsAuth] = useState(false);
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const hasToken = !!auth.getAccessToken();
-    const hasUser = !!user;
+  const hasToken = !!auth.getAccessToken();
+  const hasUser = !!user;
 
-    useEffect(() => {
-        if (!hasUser && hasToken) {
-            dispatch(getCurrentUser());
-            setIsAuth(true);
-        }
-    }, [dispatch, hasToken, hasUser]);
-    
-    return isAuth;
+  useEffect(() => {
+    if (!hasUser && hasToken) {
+      dispatch(getCurrentUser());
+      setIsAuth(true);
+    }
+  }, [dispatch, hasToken, hasUser]);
+
+  return isAuth;
 };
