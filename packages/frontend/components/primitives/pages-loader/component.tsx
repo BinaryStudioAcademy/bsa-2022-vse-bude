3ad902a0323b0f41d426134ff9cad71d1a093592
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-08-19 19:24:26 +0300

    VB-108: prietter fix

--- packages/frontend/components/primitives/pages-loader/component.tsx
+++ packages/frontend/components/primitives/pages-loader/component.tsx
@@ -1,14 +1,15 @@
 import { Router } from 'next/router';
 import { Spinner } from 'components/primitives/loader/spinner';
-import { loaderWrapper } from './styles';
 import { useState } from 'react';
+import { loaderWrapper } from './styles';
 
 export function PagesLoader() {
   const [isLoading, setIsLoading] = useState(false);
   Router.events.on('routeChangeStart', () => setIsLoading(true));
   Router.events.on('routeChangeComplete', () => setIsLoading(false));
   Router.events.on('routeChangeError', () => setIsLoading(false));
-  return (
+  
+return (
     <>
       {isLoading && (
         <div css={loaderWrapper}>
