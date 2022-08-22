417bcf835552a3001b491ba6ce4a151ba6853c09
Ivan <nikolaiev.i03@gmail.com>
2022-08-22 11:06:15 +0300

    VB-117: add item infrastructure

---
+++ packages/frontend/pages/items/[id].tsx
@@ -0,0 +1,26 @@
+﻿import { Layout } from '@components';
+import { useRouter } from 'next/router';
+import { wrapper } from 'store';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (_store) =>
+    async ({ locale }) => ({
+      props: {
+        ...(await serverSideTranslations(locale, ['common', 'footer'])),
+      },
+    }),
+);
+
+const Item = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <Layout title="Item name">
+      <div>Item id: {id}</div>
+    </Layout>
+  );
+};
+
+export default Item;
