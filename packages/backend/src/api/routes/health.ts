385debf5f195419042fc1e4f296240ff26b697f7
double-hope <nadia.prohorchuk@gmail.com>
2022-08-22 13:37:40 +0300

    VB-59: paste redisStorageService into health.ts

--- packages/backend/src/api/routes/health.ts
+++ packages/backend/src/api/routes/health.ts
@@ -1,18 +1,14 @@
 import type { ApiRoutes } from '@vse-bude/shared';
 import { Router } from 'express';
-import { initServices } from '@services';
 import type { Services } from '@services';
-import { initRepositories } from '@repositories';
 
-function initHealthRoutes({ healthService }: Services, path: ApiRoutes) {
+function initHealthRoutes({ redisStorageService, healthService }: Services, path: ApiRoutes) {
   const router = Router();
 
   return router.get(path, async (req, res) => {
     try {
       await healthService.select();
-      await initServices(
-        initRepositories(await healthService.getClient()),
-      ).redisStorageService.checkPing();
+      await redisStorageService.checkPing();
       res.status(200).send('healthy');
     } catch (error) {
       res.status(503).send('not healthy');
