f04c4814c426291a2e4444a9d7d485a63c2a7052
double-hope <nadia.prohorchuk@gmail.com>
2022-08-22 14:55:36 +0300

    VB-59: small fixes

--- packages/backend/src/repositories/health.ts
+++ packages/backend/src/repositories/health.ts
@@ -6,7 +6,7 @@ export class HealthRepository {
   constructor(prismaClient: PrismaClient) {
     this._dbClient = prismaClient;
   }
-  
+
   async select() {
     return await this._dbClient.$queryRaw`SELECT 1`;
   }
