77200abb8cf64916330592fb04e953d21bac006a
mishabenq <mixa200221@gmail.com>
2022-08-23 15:09:20 +0300

    VB-3: removed doc annotations

--- packages/backend/src/api/routes/news.ts
+++ packages/backend/src/api/routes/news.ts
@@ -11,66 +11,6 @@ export const initNewsRoutes = (
 ): Router => {
   const router = Router();
 
-  /**
-   * @openapi
-   * definitions:
-   *   News:
-   *     required:
-   *       - id
-   *       - title
-   *       - content
-   *       - description
-   *       - image
-   *       - createdAt
-   *       - updatedAt
-   *     properties:
-   *       id:
-   *         type: string
-   *       title:
-   *         type: string
-   *       content:
-   *         type: string
-   *       description:
-   *         type: string
-   *       image:
-   *         type: string
-   *       createdAt:
-   *         type: string
-   *         format: date-time
-   *       updatedAt:
-   *         type: string
-   *         format: date-time
-   */
-
-  /**
-   * @openapi
-   * /news/getAll:
-   *   get:
-   *     tags: [News]
-   *     produces:
-   *       - application/json
-   *     parameters:
-   *       - name: take
-   *         in: query
-   *         description: Amount of records.
-   *         required: true
-   *         schema:
-   *           format: double
-   *           type: number
-   *     responses:
-   *       403:
-   *         description: User is not authorized to perform the action.
-   *       200:
-   *         description: Ok
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: object
-   *               properties:
-   *                 contribution:
-   *                   $ref: "#/definitions/News"
-   */
-
   router.get(
     apiPath(path),
     wrap((req: Request) => newsService.getAll(req.query)),
