12ee31242aa16710a48040a8e38cca2208f4421c
double-hope <nadia.prohorchuk@gmail.com>
2022-08-24 16:51:06 +0300

    VB-121: add ref for localization

--- packages/frontend/components/consent/component.tsx
+++ packages/frontend/components/consent/component.tsx
@@ -1,16 +1,20 @@
 import { Modal } from '@primitives';
+// import { useTranslation } from 'next-i18next';
 import React, { useEffect, useState } from 'react';
 import { ConsentContent } from './sub-components/consent-content';
 
 export const ConsentModal = () => {
   const [isVisible, setIsVisible] = useState(false);
   useEffect(() => setIsVisible(true), []);
-
+  // const { t } = useTranslation();
+  
   return (
     <React.Fragment>
-        <Modal visible={isVisible}>
-          <ConsentContent setVisible={setIsVisible}> Our terms of service</ConsentContent>
-        </Modal>
+      <Modal visible={isVisible}>
+        <ConsentContent setVisible={setIsVisible}>
+          Our terms of service
+        </ConsentContent>
+      </Modal>
     </React.Fragment>
   );
 };
