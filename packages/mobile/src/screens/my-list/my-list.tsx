4ceaf6722f959d67d80b592fd6747722d9fe1219
natata7 <natata7@gmail.com>
2022-08-25 01:27:21 +0300

    VBM-89: add my list basic layout

--- packages/mobile/src/screens/my-list/my-list.tsx
+++ packages/mobile/src/screens/my-list/my-list.tsx
@@ -1,10 +1,33 @@
 import React, { FC } from 'react';
-import { Text, ScreenWrapper } from '~/components/components';
+import { RootScreenName } from '~/common/enums/enums';
+import { RootNavigationProps } from '~/common/types/navigation/navigation-props';
+import {
+  Text,
+  ScreenWrapper,
+  TouchableOpacity,
+  PlusIcon,
+} from '~/components/components';
+import { useCustomTheme, useNavigation, useTranslation } from '~/hooks/hooks';
+import { createStyles } from './styles';
 
 const MyList: FC = () => {
+  const navigation = useNavigation<RootNavigationProps>();
+  const { t } = useTranslation();
+  const { dark, colors } = useCustomTheme();
+
+  const styles = React.useMemo(() => createStyles(colors), [dark, colors]);
+  const handleNewItem = () => {
+    navigation.navigate(RootScreenName.NEW_ITEM);
+  };
+
   return (
     <ScreenWrapper>
       <Text>My list screen</Text>
+
+      <TouchableOpacity onPress={handleNewItem} style={styles.row}>
+        <PlusIcon size={30} style={styles.icon} />
+        <Text style={styles.btnText}>{t('make_a_post.MAKE_A_POST')}</Text>
+      </TouchableOpacity>
     </ScreenWrapper>
   );
 };
---
+++ packages/mobile/src/screens/my-list/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+import { ThemeColors } from '~/common/types/types';
+
+const createStyles = (colors: ThemeColors) => {
+  const styles = StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+      paddingVertical: 15,
+      alignItems: 'center',
+    },
+    btnText: {
+      color: colors.text,
+      fontWeight: '600',
+      fontSize: 16,
+    },
+    icon: {
+      color: colors.accent,
+      marginRight: 18,
+    },
+  });
+
+  return styles;
+};
+export { createStyles };
