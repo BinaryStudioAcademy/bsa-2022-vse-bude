43cf74a033286b797920018b1600c344bfc90fc4
Dmitriy Uvin <dimauuvin@gmail.com>
2022-08-25 22:23:56 +0300

    VB-147: split doc files to separate files

--- packages/backend/src/api/routes/category.ts
+++ packages/backend/src/api/routes/category.ts
@@ -13,32 +13,6 @@ export const initCategoryRoutes = (
 
   /**
    * @openapi
-   * definitions:
-   *   Category:
-   *     required:
-   *       - id
-   *       - title
-   *       - image
-   *       - createdAt
-   *       - updatedAt
-   *     properties:
-   *       id:
-   *         type: string
-   *       title:
-   *         type: string
-   *       createdAt:
-   *         type: string
-   *         format: date-time
-   *       image:
-   *         type: string
-   *         nullable: true
-   *       updatedAt:
-   *         type: string
-   *         format: date-time
-   */
-
-  /**
-   * @openapi
    * /categories:
    *   get:
    *     tags: [Category]
---
+++ packages/backend/src/docs/body/auth/sign-in.ts
@@ -0,0 +1,15 @@
+/**
+ * @openapi
+ * definitions:
+ *   SignInBody:
+ *     properties:
+ *       email:
+ *         type: string
+ *         required: true
+ *         example: johndoe@gmail.com
+ *       password:
+ *         type: string
+ *         required: true
+ */
+
+export {};
