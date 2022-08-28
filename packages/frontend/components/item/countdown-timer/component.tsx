fd53357badbc5dddcdb15406e1460408a6b45466
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-08-28 14:27:59 +0300

    VB-126: fix countDown timer

--- packages/frontend/components/item/countdown-timer/component.tsx
+++ packages/frontend/components/item/countdown-timer/component.tsx
@@ -1,36 +1,68 @@
-﻿import { useCountdown } from '@hooks';
+﻿import { useTimer } from '@hooks';
+import { useState } from 'react';
+import { useTranslation } from 'next-i18next';
+import type { TimerTranslations } from '@types';
+import { timeToEventObj } from 'helpers/time';
 import * as styles from './styles';
 
 interface CountDownTimerProps {
   targetDate: Date;
 }
 
+function useTimeTranslations() {
+  const { t } = useTranslation();
+  const timeTranslations: TimerTranslations = {
+    daysText: t('common:components.item.time.days'),
+    hoursText: t('common:components.item.time.hours'),
+    minsText: t('common:components.item.time.minutes'),
+    secsText: t('common:components.item.time.seconds'),
+  };
+
+  return timeTranslations;
+}
+
 export const CountDownTimer = ({ targetDate }: CountDownTimerProps) => {
-  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  const timeTranslations = useTimeTranslations();
+  const [timerValue, setTimerValue] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useTimer(() => {
+    setTimerValue(timeToEventObj(targetDate).$d);
+  });
 
   const getShownValue = (value: number) =>
     value >= 10 ? `${value}` : `0${value}`;
 
-  if (days + hours + minutes + seconds <= 0) {
+  if (
+    timerValue.days +
+      timerValue.hours +
+      timerValue.minutes +
+      timerValue.seconds <=
+    0
+  ) {
     return <div>Expired!</div>;
   } else {
     return (
       <div css={styles.wrapper}>
         <div>
-          <span>{getShownValue(days)}</span>
-          <span>days</span>
+          <span>{getShownValue(timerValue.days)}</span>
+          <span>{timeTranslations.daysText}</span>
         </div>
         <div>
-          <span>{getShownValue(hours)}</span>
-          <span>hours</span>
+          <span>{getShownValue(timerValue.hours)}</span>
+          <span>{timeTranslations.hoursText}</span>
         </div>
         <div>
-          <span>{getShownValue(minutes)}</span>
-          <span>min</span>
+          <span>{getShownValue(timerValue.minutes)}</span>
+          <span>{timeTranslations.minsText}</span>
         </div>
         <div>
-          <span>{getShownValue(seconds)}</span>
-          <span>sec</span>
+          <span>{getShownValue(timerValue.seconds)}</span>
+          <span>{timeTranslations.secsText}</span>
         </div>
       </div>
     );
--- packages/frontend/hooks/index.ts
+++ packages/frontend/hooks/index.ts
@@ -1,7 +1,6 @@
 export * from './store';
 export * from './use-outside-click';
 export * from './time';
-export * from './countDownTimer';
 export * from './use-auth';
 export * from './use-mounted';
 export * from './use-window-size';
