b4143d14e1de9af090d8b2efac344fd4286a0636
double-hope <nadia.prohorchuk@gmail.com>
2022-08-28 15:59:39 +0300

    VB-119: fix prettier

--- packages/frontend/pages/items/filter.tsx
+++ packages/frontend/pages/items/filter.tsx
@@ -7,17 +7,17 @@ import { useTypedSelector } from '@hooks';
 
 export const getServerSideProps = withPublic(
   wrapper.getServerSideProps((_store) => async ({ locale }) => ({
-      props: {
-        ...(await serverSideTranslations(locale, ['common'])),
-      },
-    })),
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  })),
 );
 
 const FilteredPage = () => {
   const router = useRouter();
   const filter = router.query.filter as string;
   const { list } = useTypedSelector((state) => state.product);
-  
+
   return (
     <Layout title="Filtered posts">
       <Filter filter={filter} lots={list} />
