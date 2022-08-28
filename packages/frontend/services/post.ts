692d16e7cbaf80d5406fff8816431038172136c3
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-08-28 22:20:34 +0300

    VB-124: fixed type

--- packages/frontend/services/post.ts
+++ packages/frontend/services/post.ts
@@ -1,9 +1,9 @@
 import { http } from '@helpers';
-import { ApiRoutes, HttpContentType, type ICreatePost } from '@vse-bude/shared';
+import { ApiRoutes, HttpContentType } from '@vse-bude/shared';
 
 export const getPostById = () => Promise.resolve();
 
-export const createPost = (_data: ICreatePost) => Promise.resolve();
+export const createPost = (_data: FormData) => Promise.resolve();
 
 export const uploadImage = (formData: FormData) =>
   http.post({
