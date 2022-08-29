b3335a388a849d7941ee3018252b1930418e2cf1
natata7 <natata7@gmail.com>
2022-08-29 16:46:27 +0300

    VBM-42: fix naming

--- packages/mobile/src/helpers/helpers.ts
+++ packages/mobile/src/helpers/helpers.ts
@@ -1,3 +1,3 @@
 export { getQueryString } from './http/http';
 export { pickImageLibrary, pickImageCamera } from './image/image';
-export { requestExternalStoragePermission } from './permissions/android-permissions';
+export { requestExternalStoragePermission } from './permissions/permissions';
