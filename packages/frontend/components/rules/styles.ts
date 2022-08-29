538e78bcdcf75f5174341b1c0776c20f561b7da1
yevhenii-moroziuk <kemd99@gmail.com>
2022-08-29 14:20:00 +0300

    Add rules page

---
+++ packages/frontend/components/rules/styles.ts
@@ -0,0 +1,9 @@
+import type { Theme } from "@emotion/react";
+import { css } from "@emotion/react";
+
+export const heading = ({ fontSizes, fontWeights, lineHeights }: Theme) => css`
+  margin: 0;
+  font-size: ${fontSizes.h3};
+  font-weight: ${fontWeights.h3};
+  line-height: ${lineHeights.h3};
+`;
