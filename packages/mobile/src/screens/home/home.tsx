ea8caadd4b5ed98675760d60169e689e23b97490
natata7 <natata7@gmail.com>
2022-08-30 12:55:08 +0300

    VBM-117: fix prettier

--- packages/mobile/src/screens/home/home.tsx
+++ packages/mobile/src/screens/home/home.tsx
@@ -8,7 +8,13 @@ const Home: FC = () => {
     <ScreenWrapper style={[globalStyles.justifyContentSpaceBetween]}>
       <Text>Home screen</Text>
 
-      <View style={[globalStyles.flexDirectionRow, globalStyles.justifyContentCenter, globalStyles.py3]}>
+      <View
+        style={[
+          globalStyles.flexDirectionRow,
+          globalStyles.justifyContentCenter,
+          globalStyles.py3,
+        ]}
+      >
         <Text style={[globalStyles.fs12, globalStyles.mr1]}>BuildNumber:</Text>
         <Text style={[globalStyles.fs12, globalStyles.mr5]}>
           {DeviceInfo.getBuildNumber()}
