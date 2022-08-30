760498e04c608f0916c7f8caa200b877b6248513
natata7 <natata7@gmail.com>
2022-08-30 13:21:59 +0300

    VBM-117: fixed component

--- packages/mobile/src/screens/home/home.tsx
+++ packages/mobile/src/screens/home/home.tsx
@@ -1,27 +1,12 @@
 import React, { FC } from 'react';
-import { Text, ScreenWrapper, View } from '~/components/components';
-import DeviceInfo from 'react-native-device-info';
+import { Text, ScreenWrapper, BuildInfo } from '~/components/components';
 import { globalStyles } from '~/styles/styles';
 
 const Home: FC = () => {
   return (
-    <ScreenWrapper style={[globalStyles.justifyContentSpaceBetween]}>
+    <ScreenWrapper style={globalStyles.justifyContentSpaceBetween}>
       <Text>Home screen</Text>
-
-      <View
-        style={[
-          globalStyles.flexDirectionRow,
-          globalStyles.justifyContentCenter,
-          globalStyles.py3,
-        ]}
-      >
-        <Text style={[globalStyles.fs12, globalStyles.mr1]}>BuildNumber:</Text>
-        <Text style={[globalStyles.fs12, globalStyles.mr5]}>
-          {DeviceInfo.getBuildNumber()}
-        </Text>
-        <Text style={[globalStyles.fs12, globalStyles.mr1]}>Version:</Text>
-        <Text style={globalStyles.fs12}>{DeviceInfo.getVersion()}</Text>
-      </View>
+      <BuildInfo />
     </ScreenWrapper>
   );
 };
