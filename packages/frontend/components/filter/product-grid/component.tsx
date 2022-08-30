6f79b703f3b7a6510524d0529f20e90546536a01
double-hope <nadia.prohorchuk@gmail.com>
2022-08-30 10:03:46 +0300

    VB-119: fix prettier

--- packages/frontend/components/filter/product-grid/component.tsx
+++ packages/frontend/components/filter/product-grid/component.tsx
@@ -10,25 +10,25 @@ export const ProductGrid = ({ lots }: ProductGridProps) => {
   const handleBidBuyProduct = (productId: string) => {
     router.push(`${Routes.ITEMS}/${productId}`);
   };
-  
-  return(
-  <div css={styles.productGridWrapper}>
-    <div css={styles.productGrid}>
-      {lots.map((item) => (
-        <SplideSlide key={item.id}>
-          <ProductCard
-            data={item}
-            name={item.title}
-            description={item.description}
-            price={item.price}
-            images={item.imageLinks}
-            currency="UAH"
-            auctionDate={item.endDate}
-            onButtonClick={handleBidBuyProduct}
-          />
-        </SplideSlide>
-      ))}
+
+  return (
+    <div css={styles.productGridWrapper}>
+      <div css={styles.productGrid}>
+        {lots.map((item) => (
+          <SplideSlide key={item.id}>
+            <ProductCard
+              data={item}
+              name={item.title}
+              description={item.description}
+              price={item.price}
+              images={item.imageLinks}
+              currency="UAH"
+              auctionDate={item.endDate}
+              onButtonClick={handleBidBuyProduct}
+            />
+          </SplideSlide>
+        ))}
+      </div>
     </div>
-  </div>
   );
 };
