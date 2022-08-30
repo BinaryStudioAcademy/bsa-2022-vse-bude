efa17901c727898dd500ac1f6c52a9c0c2d11efc
natata7 <natata7@gmail.com>
2022-08-30 12:43:31 +0300

    VBM-117: add build & version number on home screen

--- packages/mobile/src/screens/home/home.tsx
+++ packages/mobile/src/screens/home/home.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from 'react';
-import { Text, ScreenWrapper } from '~/components/components';
+import { Text, ScreenWrapper, View } from '~/components/components';
+import DeviceInfo from 'react-native-device-info';
+import { globalStyles } from '~/styles/styles';
 
 const Home: FC = () => {
   return (
-    <ScreenWrapper>
+    <ScreenWrapper style={[globalStyles.justifyContentSpaceBetween]}>
       <Text>Home screen</Text>
+
+      <View style={[globalStyles.flexDirectionRow]}>
+        <Text style={[globalStyles.fs12, globalStyles.mr1]}>BuildNumber:</Text>
+        <Text style={[globalStyles.fs12, globalStyles.mr5]}>
+          {DeviceInfo.getBuildNumber()}
+        </Text>
+        <Text style={[globalStyles.fs12, globalStyles.mr1]}>Version:</Text>
+        <Text style={globalStyles.fs12}>{DeviceInfo.getVersion()}</Text>
+      </View>
     </ScreenWrapper>
   );
 };
