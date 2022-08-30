161330340c316479a3fd862818eb9dc2b1539b2d
double-hope <nadia.prohorchuk@gmail.com>
2022-08-30 12:49:36 +0300

    VB-119: fetch products from query

--- packages/frontend/components/filter/component.tsx
+++ packages/frontend/components/filter/component.tsx
@@ -2,13 +2,11 @@ import type { ProductDto } from '@vse-bude/shared';
 import { ProductGrid } from './product-grid/component';
 
 interface FilterProps {
-  filter: string;
   lots: ProductDto[];
 }
 
-export const Filter = ({ filter, lots }: FilterProps) => (
+export const Filter = ({ lots }: FilterProps) => (
   <div>
-    {filter}
     <ProductGrid lots={lots}></ProductGrid>
   </div>
 );
--- packages/frontend/pages/items/filter.tsx
+++ packages/frontend/pages/items/filter.tsx
@@ -4,7 +4,6 @@ import { wrapper } from 'store';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useAppDispatch, useTypedSelector } from '@hooks';
 import { fetchProducts } from 'store/product';
-import { ProductType } from '@vse-bude/shared';
 import { useEffect } from 'react';
 
 export const getServerSideProps = wrapper.getServerSideProps(
@@ -18,17 +17,18 @@ export const getServerSideProps = wrapper.getServerSideProps(
 
 const FilteredPage = () => {
   const router = useRouter();
-  const filter = router.query.filter as string;
+  const query = router.query as unknown as string;
   const { list } = useTypedSelector((store) => store.product);
+  const filter = Object.fromEntries(new URLSearchParams(query));
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchProducts({ limit: 5, type: ProductType.AUCTION }));
-  }, [dispatch]);
+    dispatch(fetchProducts(filter));
+  }, [dispatch, router.query, filter]);
 
   return (
     <Layout title="Filtered posts">
-      <Filter filter={filter} lots={list} />
+      <Filter lots={list} />
     </Layout>
   );
 };
