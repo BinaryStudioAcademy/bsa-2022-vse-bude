e7aa07ba97e163a52aa913278a6faa7181aeb3a3
natata7 <natata7@gmail.com>
2022-09-01 00:06:32 +0300

    VBM-89: fix naming

--- packages/mobile/src/screens/my-list/my-list.tsx
+++ packages/mobile/src/screens/my-list/my-list.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { RootScreenName } from '~/common/enums/enums';
-import { RootNavigationProps } from '~/common/types/navigation/navigation-props';
+import { RootNavigationProps } from '~/common/types/types';
 import {
   Text,
   ScreenWrapper,
@@ -12,6 +12,7 @@ import {
   useCustomTheme,
   useNavigation,
   useTranslation,
+  useMemo,
 } from '~/hooks/hooks';
 import { selectCurrentUser } from '~/store/selectors';
 import { createStyles } from './styles';
@@ -22,8 +23,8 @@ const MyList: FC = () => {
   const { dark, colors } = useCustomTheme();
   const user = useAppSelector(selectCurrentUser);
 
-  const styles = React.useMemo(() => createStyles(colors), [dark, colors]);
-  const handleNewItem = () => {
+  const styles = useMemo(() => createStyles(colors), [dark, colors]);
+  const handleCreateNewItemPress = () => {
     navigation.navigate(RootScreenName.NEW_ITEM);
   };
 
@@ -31,7 +32,7 @@ const MyList: FC = () => {
     <ScreenWrapper style={styles.screen}>
       <Text>My list screen</Text>
       {user && (
-        <TouchableOpacity onPress={handleNewItem} style={styles.row}>
+        <TouchableOpacity onPress={handleCreateNewItemPress} style={styles.row}>
           <PlusIcon size={30} style={styles.icon} />
           <Text style={styles.btnText}>{t('make_a_post.TITLE')}</Text>
         </TouchableOpacity>
