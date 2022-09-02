824f54a0eb4a6ab25822f588bedcd8022de4cdfb
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-09-02 18:08:04 +0300

    VBM-60:remove img import

--- packages/mobile/src/assets/images/images.ts
+++ packages/mobile/src/assets/images/images.ts
@@ -5,7 +5,6 @@ const images = {
   toys: require('~/assets/images/category/toys.png'),
   home_decor: require('~/assets/images/category/home-decor.png'),
   appliances: require('~/assets/images/category/appliances.png'),
-  picture: require('~/assets/images/lot/picture.png'),
   prytula: require('~/assets/images/organizations/prytula.png'),
   come_back_alive: require('~/assets/images/organizations/come-back-alive.png'),
   help_children: require('~/assets/images/organizations/help-children.png'),
