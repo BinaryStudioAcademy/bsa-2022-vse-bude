828e8be22f57aa06a2095d7f1dadfa9e7e24d0ba
natata7 <natata7@gmail.com>
2022-09-03 23:51:36 +0300

    VBM-119: add screen

---
+++ packages/mobile/src/screens/type-of-post/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+import { useCustomTheme, useMemo } from '~/hooks/hooks';
+
+const useStyles = () => {
+  const { dark, colors } = useCustomTheme();
+
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        screen: {
+          backgroundColor: colors.background,
+          padding: 20,
+        },
+        image: {
+          width: 267,
+          height: 148,
+          resizeMode: 'contain',
+          alignSelf: 'center',
+        },
+      }),
+    [dark, colors],
+  );
+};
+
+export { useStyles };
