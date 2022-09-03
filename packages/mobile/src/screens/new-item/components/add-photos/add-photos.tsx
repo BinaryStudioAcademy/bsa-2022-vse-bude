ffd83084f4bba1af9a94c4c90bdfae1486f5d2e4
natata7 <natata7@gmail.com>
2022-09-03 14:03:30 +0300

    VBM-118: add uploading photo

--- packages/mobile/src/screens/new-item/components/add-photos/add-photos.tsx
+++ packages/mobile/src/screens/new-item/components/add-photos/add-photos.tsx
@@ -1,28 +1,94 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback } from 'react';
+import { image as imageService } from '~/services/services';
 import {
   View,
   ImageIcon,
   TouchableOpacity,
   Text,
+  ScrollView,
+  Image,
+  CrossIcon,
 } from '~/components/components';
+import { pickImageLibrary } from '~/helpers/helpers';
 import { useTranslation } from '~/hooks/hooks';
 import { globalStyles } from '~/styles/styles';
+import { ColorPalette } from '@vse-bude/shared';
 import { styles } from './styles';
 
 const AddPhotos: FC = () => {
   const { t } = useTranslation();
+  const [images, setImages] = useState<string[]>([]);
+  const [, setIsUploading] = useState(false);
+
+  const onGalleryOpen = useCallback(async () => {
+    setIsUploading(true);
+    const file = await pickImageLibrary();
+    if (!file) {
+      setIsUploading(false);
+
+      return;
+    }
+    const link = await imageService.uploadImage(file);
+
+    setImages((state) => [...state, link]);
+    setIsUploading(false);
+  }, []);
+
+  const deleteImage = (index: number) => {
+    const updateImages = [...images];
+    updateImages.splice(index, 1);
+    setImages(updateImages);
+  };
+
+  const renderImage = (image: string, index: number) => {
+    return (
+      <View
+        style={[
+          styles.btnWrapper,
+          globalStyles.px3,
+          globalStyles.py3,
+          globalStyles.mt5,
+          globalStyles.mr5,
+        ]}
+      >
+        <Image source={{ width: 80, height: 80, uri: image }} />
+
+        <TouchableOpacity
+          style={[
+            styles.deleteButton,
+            globalStyles.alignItemsCenter,
+            globalStyles.justifyContentCenter,
+          ]}
+          onPress={() => deleteImage(index)}
+          key={index}
+        >
+          <CrossIcon size={20} color={ColorPalette.RED_100} />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
-    <TouchableOpacity
-      style={[styles.btnWrapper, globalStyles.px5, globalStyles.mt5]}
-    >
-      <View style={styles.btnContainer}>
-        <ImageIcon size={16} style={styles.icon} />
-        <Text style={[globalStyles.fs14, styles.btnText]}>
-          {t('make_a_post.ADD_PHOTOS')}
-        </Text>
+    <>
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {images.map((imageItem, index) => {
+            return renderImage(imageItem, index);
+          })}
+        </ScrollView>
       </View>
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={onGalleryOpen}
+        style={[styles.btnWrapper, globalStyles.px5, globalStyles.mt5]}
+      >
+        <View style={styles.btnContainer}>
+          <ImageIcon size={16} style={styles.icon} />
+          <Text style={[globalStyles.fs14, styles.btnText]}>
+            {t('make_a_post.ADD_PHOTOS')}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </>
   );
 };
 
