6974b151de45df0bd503c94ed41fc2b8ccf9864a
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-09-06 23:00:33 +0300

    VB-240: move condition translations to product file

--- packages/backend/src/helpers/product.ts
+++ packages/backend/src/helpers/product.ts
@@ -3,6 +3,6 @@ import type { Condition } from '@prisma/client';
 
 export const translateCondition = (condition: Condition) =>
   ({
-    NEW: lang('product-condition:NEW'),
-    USED: lang('product-condition:USED'),
+    NEW: lang('product:condition.NEW'),
+    USED: lang('product:condition.USED'),
   }[condition]);
