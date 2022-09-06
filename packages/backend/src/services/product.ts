d278e893894103315bc80ef1fad69a1b56ca9279
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-09-06 22:50:01 +0300

    refactor localization condition

--- packages/backend/src/services/product.ts
+++ packages/backend/src/services/product.ts
@@ -6,7 +6,12 @@ import {
   AuctionEndedError,
 } from '@errors';
 import type { Request } from 'express';
-import { getFilenameFromUrl, getUserIdFromRequest, toUtc } from '@helpers';
+import {
+  getFilenameFromUrl,
+  getUserIdFromRequest,
+  toUtc,
+  translateCondition,
+} from '@helpers';
 import type {
   AddProductToFavorites,
   AuctionPermissionsResponse,
@@ -16,18 +21,15 @@ import type {
   UpdateProduct,
 } from '@vse-bude/shared';
 import { ProductType } from '@vse-bude/shared';
-import type { Product } from '@prisma/client';
-import type { Bid } from '@prisma/client';
 import { ProductStatus } from '@prisma/client';
-import type { VerifyService } from '@services';
+import type { Product, Bid } from '@prisma/client';
+import type { VerifyService, S3StorageService } from '@services';
 import type { BidRepository } from '@repositories';
-import { productMapper } from '@mappers';
+import { productMapper, auctionPermissionsMapper } from '@mappers';
 import { FieldError } from 'error/product/field-error';
 import { createPostSchema, updatePostSchema } from 'validation/product/schemas';
-import { auctionPermissionsMapper } from '@mappers';
 import { NotVerifiedError } from 'error/user/not-verified';
-import { lang } from '../lang';
-import type { S3StorageService } from './s3-storage';
+import { lang } from '@lang';
 
 export class ProductService {
   private _productRepository: ProductRepository;
@@ -65,7 +67,7 @@ export class ProductService {
     }
 
     if (product.condition) {
-      product.condition = lang(`product-condition:${product.condition}`);
+      product.condition = translateCondition(product.condition);
     }
 
     const currentPrice = await this._productRepository.getCurrentPrice(
