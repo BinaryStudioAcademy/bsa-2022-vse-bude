48003612443e62dc7c2ac6fe603d7ce89f014706
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-09-06 20:26:40 +0300

    VB-203: added notVerificatedWarning

--- packages/frontend/components/save-post/component.tsx
+++ packages/frontend/components/save-post/component.tsx
@@ -1,9 +1,9 @@
 import { useTranslation } from 'next-i18next';
 import { Container } from '@primitives';
 import { ProductType } from '@vse-bude/shared';
-import { useAppDispatch, useTypedSelector } from '@hooks';
-import { useEffect } from 'react';
-import { addToast } from 'store/toast/actions';
+import { useTypedSelector } from '@hooks';
+import NotVerificatedWarning from '@components/verification/not-verificated-warning/component';
+import { IsVerificatedSelector } from 'store/auth';
 import { NestedLayout } from '../profile/user-account/common';
 import type { SavePostProps } from './types';
 import AuctionForm from './auction-form';
@@ -12,24 +12,15 @@ import * as styles from './styles';
 
 export const SavePost = ({ type, edit }: SavePostProps) => {
   const { t } = useTranslation();
-  const dispatch = useAppDispatch();
-  const user = useTypedSelector((state) => state.auth.user);
-
-  useEffect(() => {
-    if (!user) return;
-    if (!user.phoneVerified || !user.emailVerified) {
-      dispatch(
-        addToast({
-          level: 'warning',
-          description: t('create-post:notification.notVerified'),
-        }),
-      );
-    }
-  }, [t, dispatch, user]);
+  const isVerificated = useTypedSelector(IsVerificatedSelector);
 
   const title = edit
     ? t('create-post:headline.editPost')
     : t('create-post:headline.makePost');
+
+  if (!isVerificated) {
+    return <NotVerificatedWarning />;
+  }
 
   return (
     <NestedLayout>
