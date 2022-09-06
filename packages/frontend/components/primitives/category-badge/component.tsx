cf0737edf5581a3381a5d3b26baf894d37d317fd
mishabenq <mixa200221@gmail.com>
2022-09-06 21:46:00 +0300

    VB-301: added proper aria label

--- packages/frontend/components/primitives/category-badge/component.tsx
+++ packages/frontend/components/primitives/category-badge/component.tsx
@@ -1,43 +1,48 @@
 ﻿import { IconColor, IconName } from '@enums';
 import { Container, IconButton } from '@primitives';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { useTranslation } from 'next-i18next';
 import * as styles from './styles';
 
 interface CategoryBadgesProps {
   badges: {
     name: string;
-    onClick?: () => void;
+    onClick: () => void;
   }[];
 }
 
-export const CategoryBadges = ({ badges }: CategoryBadgesProps) => (
-  <Container>
-    <Splide
-      options={{
-        autoWidth: true,
-        pagination: false,
-        arrows: false,
-        wheel: true,
-        releaseWheel: true,
-        mediaQuery: 'max',
-        breakpoints: {},
-      }}
-    >
-      {badges.map((badge) => (
-        <SplideSlide key={badge.name}>
-          <div css={styles.badge} key={badge.name}>
-            {badge.name}
-            <IconButton
-              icon={IconName.XMARK}
-              size="xs"
-              onClick={badge.onClick}
-              color={IconColor.GRAY}
-              ariaLabel={badge.name + '_xmark'}
-              cssExtend={styles.cross}
-            />
-          </div>
-        </SplideSlide>
-      ))}
-    </Splide>
-  </Container>
-);
+export const CategoryBadges = ({ badges }: CategoryBadgesProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <Splide
+        options={{
+          autoWidth: true,
+          pagination: false,
+          arrows: false,
+          wheel: true,
+          releaseWheel: true,
+          mediaQuery: 'max',
+          breakpoints: {},
+        }}
+      >
+        {badges.map((badge) => (
+          <SplideSlide key={badge.name}>
+            <div css={styles.badge} key={badge.name}>
+              {badge.name}
+              <IconButton
+                icon={IconName.XMARK}
+                size="xs"
+                onClick={badge.onClick}
+                color={IconColor.GRAY}
+                ariaLabel={t(`item:remove`) + ` ${badge.name}`}
+                cssExtend={styles.cross}
+              />
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </Container>
+  );
+};
