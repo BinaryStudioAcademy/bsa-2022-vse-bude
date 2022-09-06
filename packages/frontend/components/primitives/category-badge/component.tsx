22f17903dd58506da9f8cbb21fcb080eb338aced
mishabenq <mixa200221@gmail.com>
2022-09-06 20:38:03 +0300

    VB-301: created category badge

---
+++ packages/frontend/components/primitives/category-badge/component.tsx
@@ -0,0 +1,43 @@
+﻿import { IconColor, IconName } from '@enums';
+import { Container, IconButton } from '@primitives';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import * as styles from './styles';
+
+interface CategoryBadgesProps {
+  badges: {
+    name: string;
+  }[];
+  onClick?: () => void;
+}
+
+export const CategoryBadges = ({ badges, onClick }: CategoryBadgesProps) => (
+  <Container>
+    <Splide
+      options={{
+        autoWidth: true,
+        pagination: false,
+        arrows: false,
+        wheel: true,
+        releaseWheel: true,
+        mediaQuery: 'max',
+        breakpoints: {},
+      }}
+    >
+      {badges.map((badge) => (
+        <SplideSlide key={badge.name}>
+          <div css={styles.badge} key={badge.name}>
+            {badge.name}
+            <IconButton
+              icon={IconName.XMARK}
+              size="xs"
+              onClick={onClick}
+              color={IconColor.GRAY}
+              ariaLabel={badge.name + '_xmark'}
+              cssExtend={styles.cross}
+            />
+          </div>
+        </SplideSlide>
+      ))}
+    </Splide>
+  </Container>
+);
