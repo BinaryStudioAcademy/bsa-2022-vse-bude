42231e72da742c1ec4866136be9dd904b9c0671b
Vladimir <vladimir.bloodov@gmail.com>
2022-09-07 14:45:16 +0300

    fix

--- packages/mobile/src/store/actions.ts
+++ packages/mobile/src/store/actions.ts
@@ -1,7 +1,4 @@
 export * as auth from './auth/actions';
 export * as products from './products/actions';
-<<<<<<< HEAD
 export * as verifyPhoneActions from './verify-phone/actions';
-=======
 export * as categories from './categories/action';
->>>>>>> main
