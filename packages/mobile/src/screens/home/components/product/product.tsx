f9f0926da1a88fe0c82ce3b84509dea8815cd4c0
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-09-08 14:45:50 +0300

    VBM-140: don"t display inactive products

--- packages/mobile/src/screens/home/components/product/product.tsx
+++ packages/mobile/src/screens/home/components/product/product.tsx
@@ -50,74 +50,80 @@ const Product: FC<Props> = ({ productId }) => {
   };
 
   return (
-    <TouchableOpacity
-      onPress={handleOpenProductInfo}
-      style={[styles.container, globalStyles.boxShadow]}
-    >
-      <View style={styles.imgWrapper}>
-        <Image source={{ uri: imageLinks[0] }} style={styles.img} />
+    <>
+      {isActive && (
+        <TouchableOpacity
+          onPress={handleOpenProductInfo}
+          style={[styles.container, globalStyles.boxShadow]}
+        >
+          <View style={styles.imgWrapper}>
+            <Image source={{ uri: imageLinks[0] }} style={styles.img} />
 
-        {isAuction && isActive && (
-          <TimeWindow
-            duration={timeToAuctionEnd}
-            style={{ position: 'absolute', bottom: -10, alignSelf: 'center' }}
-          />
-        )}
-      </View>
-      <View>
-        <Text
-          style={[
-            globalStyles.fs16,
-            globalStyles.fontWeightBold,
-            styles.title,
-            { color: colors.titleSecondary },
-          ]}
-          numberOfLines={1}
-        >
-          {title}
-        </Text>
-        <Text
-          style={[globalStyles.fs14, globalStyles.fontWeightRegular]}
-          numberOfLines={2}
-        >
-          {description}
-        </Text>
-        {!isAuction && (
-          <Text style={[globalStyles.fs12, globalStyles.mt2]}>
-            {createdAtDate}
-          </Text>
-        )}
-      </View>
-      <View style={[styles.divider, { backgroundColor: colors.line }]} />
-      <View
-        style={[
-          globalStyles.flexDirectionRow,
-          globalStyles.alignItemsCenter,
-          globalStyles.justifyContentSpaceBetween,
-          globalStyles.flex1,
-        ]}
-      >
-        <Text
-          style={[
-            globalStyles.fs16,
-            globalStyles.fontWeightBold,
-            { color: colors.titleSecondary },
-          ]}
-        >{`${formatPrice(price)} ${t('common:currency.UAH')}`}</Text>
-        {isActive && (
-          <Button
-            label={
-              isAuction
-                ? t('common:components.BUTTON_BID')
-                : t('common:components.BUTTON_BUY')
-            }
-            onPress={() => {
-              //TODO
-            }}
-          ></Button>
-        )}
-      </View>
-    </TouchableOpacity>
+            {isAuction && (
+              <TimeWindow
+                duration={timeToAuctionEnd}
+                style={{
+                  position: 'absolute',
+                  bottom: -10,
+                  alignSelf: 'center',
+                }}
+              />
+            )}
+          </View>
+          <View>
+            <Text
+              style={[
+                globalStyles.fs16,
+                globalStyles.fontWeightBold,
+                styles.title,
+                { color: colors.titleSecondary },
+              ]}
+              numberOfLines={1}
+            >
+              {title}
+            </Text>
+            <Text
+              style={[globalStyles.fs14, globalStyles.fontWeightRegular]}
+              numberOfLines={2}
+            >
+              {description}
+            </Text>
+            {!isAuction && (
+              <Text style={[globalStyles.fs12, globalStyles.mt2]}>
+                {createdAtDate}
+              </Text>
+            )}
+          </View>
+          <View style={[styles.divider, { backgroundColor: colors.line }]} />
+          <View
+            style={[
+              globalStyles.flexDirectionRow,
+              globalStyles.alignItemsCenter,
+              globalStyles.justifyContentSpaceBetween,
+              globalStyles.flex1,
+            ]}
+          >
+            <Text
+              style={[
+                globalStyles.fs16,
+                globalStyles.fontWeightBold,
+                { color: colors.titleSecondary },
+              ]}
+            >{`${formatPrice(price)} ${t('common:currency.UAH')}`}</Text>
+            <Button
+              label={
+                isAuction
+                  ? t('common:components.BUTTON_BID')
+                  : t('common:components.BUTTON_BUY')
+              }
+              onPress={() => {
+                //TODO
+              }}
+            ></Button>
+          </View>
+        </TouchableOpacity>
+      )}
+    </>
   );
 };
 
