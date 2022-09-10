643b94e400ae2231c4a89811e8a492bd63324ac7
natata7 <natata7@gmail.com>
2022-09-10 21:57:25 +0300

    VBM-142: add action

--- packages/mobile/src/store/product/actions.ts
+++ packages/mobile/src/store/product/actions.ts
@@ -13,4 +13,14 @@ const loadProductInfo = createAsyncThunk<ItemDto, string, AsyncThunkConfig>(
   },
 );
 
-export { loadProductInfo };
+const updateProductViews = createAsyncThunk<ItemDto, string, AsyncThunkConfig>(
+  ActionType.INCREMENT_PRODUCT_VIEWS,
+  async (productId, { extra }) => {
+    const { productApi } = extra;
+    const response = await productApi.incrementProductViews(productId);
+    
+    return response;
+  },
+);
+
+export { loadProductInfo, updateProductViews };
