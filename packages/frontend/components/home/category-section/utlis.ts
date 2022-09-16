a5e198673e222551111b8890df78fd3ad3aee669
mishabenq <mixa200221@gmail.com>
2022-09-16 12:47:39 +0300

    VB-407: added images to categories

--- packages/frontend/components/home/category-section/utlis.ts
+++ packages/frontend/components/home/category-section/utlis.ts
@@ -1,9 +1,47 @@
 const categoryImageById = {
-  '51dea135-fca2-4cc0-ab5b-a32c909f14e7': '/images/categories/appliances.png',
-  '50d4f365-e241-4783-8b2c-809a7d83771b': '/images/categories/appliances.png',
-  '3f9bd5c0-f9a8-4763-b020-eac58681b1cf': '/images/categories/toys.png',
-  '0e86f866-2146-4a5a-970b-7a62f07144a6': '/images/categories/decor.png',
-  'beff46f0-133b-4af7-8206-83419b526734': '/images/categories/arts.png',
+  //LAPTOPS_AND_PC_CATEGORY_NAME
+  '51dea135-fca2-4cc0-ab5b-a32c909f14e7':
+    '/images/categories/LAPTOPS_AND_PC.png',
+  //SMARTPHONES_TV_AND_ELECTRONICS_CATEGORY_NAME
+  '50d4f365-e241-4783-8b2c-809a7d83771b':
+    '/images/categories/SMARTPHONES_TV_AND_ELECTRONICS.png',
+  //GOODS_FOR_GAMERS_CATEGORY_NAME
+  '3f9bd5c0-f9a8-4763-b020-eac58681b1cf':
+    '/images/categories/GOODS_FOR_GAMERS.png',
+  //HOUSEHOLD_APPLIANCES_CATEGORY_NAME
+  '0e86f866-2146-4a5a-970b-7a62f07144a6': '/images/categories/appliances.png',
+  //HOME_GOODS_CATEGORY_NAME
+  'a7694c34-2325-4d0d-b344-0fe074e99862': '/images/categories/HOME_GOODS.png',
+  //TOOLS_AND_AUTO_GOODS_CATEGORY_NAME
+  '6b8df3e5-b587-413f-9943-ef3d695b97c0':
+    '/images/categories/TOOLS_AND_AUTO_GOODS.png',
+  //PLUMBING_AND_REPAIR_CATEGORY_NAME
+  'e4bef890-791e-4c53-adfd-c455227b54ad':
+    '/images/categories/PLUMBING_AND_REPAIR.png',
+  //COTTAGE_GARDEN_AND_VEGETABLE_GARDEN_CATEGORY_NAME
+  'beff46f0-133b-4af7-8206-83419b526734':
+    '/images/categories/COTTAGE_GARDEN_AND_VEGETABLE_GARDEN.png',
+  //SPORTS_AND_HOBBY_CATEGORY_NAME
+  'a351b320-76d7-4545-bb74-8bb1f2fa486d':
+    '/images/categories/SPORTS_AND_HOBBY.png',
+  //CLOTHES_SHOES_AND_JEWELRY_CATEGORY_NAME
+  '0bfc0f51-e082-4dc9-b545-ae0afedbd330':
+    '/images/categories/CLOTHES_SHOES_AND_JEWELRY.png',
+  //BEAUTY_AND_HEALTH_CATEGORY_NAME
+  '8556c563-4b04-43cb-8fe1-5bc53b5f0aad':
+    '/images/categories/BEAUTY_AND_HEALTH.png',
+  //CHILDRENS_GOODS_CATEGORY_NAME
+  '948feb22-e480-4e24-a90d-e49aa3d990c2': '/images/categories/toys.png',
+  //PET_PRODUCTS_CATEGORY_NAME
+  '54fa7b5f-58d1-4e5d-b588-d9a8507fa3c7': '/images/categories/PET_PRODUCTS.png',
+  //OFFICE_SCHOOL_BOOKS_CATEGORY_NAME
+  '410c606f-f463-42ec-988e-d95917dd2834':
+    '/images/categories/OFFICE_SCHOOL_BOOKS.png',
+  //ALCOHOLIC_BEVERAGES_AND_PRODUCTS_CATEGORY_NAME
+  '7062243f-f114-4b5a-abea-64fc21f4e117':
+    '/images/categories/ALCOHOLIC_BEVERAGES_AND_PRODUCTS.png',
+  //SERVICES_CATEGORY_NAME
+  'f5750398-bdfa-4262-adbb-c1e1c4cc92eb': '/images/categories/SERVICES.png',
 };
 
 const DEFAULT_IMAGE = '/images/categories/decor.png';
