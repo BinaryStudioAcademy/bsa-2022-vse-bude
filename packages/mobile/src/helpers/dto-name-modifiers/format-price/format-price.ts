ae1bc5a019eeb91757aea35967caa3e663156a64
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-09-18 22:00:14 +0300

    VBM-200: add 4 numbers display format

--- packages/mobile/src/helpers/dto-name-modifiers/format-price/format-price.ts
+++ packages/mobile/src/helpers/dto-name-modifiers/format-price/format-price.ts
@@ -1,9 +1,13 @@
 const formatPrice = (price: number | string): string => {
   const priceAsString = price.toString();
+  if (priceAsString.length > 4) {
+    return `${priceAsString.slice(0, 2)} ${priceAsString.slice(2)}`;
+  }
+  if (priceAsString.length === 4) {
+    return `${priceAsString.slice(0, 1)} ${priceAsString.slice(1)}`;
+  }
 
-  return priceAsString.length > 3
-    ? `${priceAsString.slice(0, 2)} ${priceAsString.slice(2)}`
-    : priceAsString;
+  return priceAsString;
 };
 
 export { formatPrice };
