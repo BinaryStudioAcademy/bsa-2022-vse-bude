e7c1005e9c0a4792f1944422790f1ee4f2492387
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2022-09-18 16:47:00 +0300

    fix/layout-persinfo: fixed reset store state

--- packages/frontend/components/profile/user-account/personal-info/component.tsx
+++ packages/frontend/components/profile/user-account/personal-info/component.tsx
@@ -4,10 +4,11 @@ import { shallowEqual } from 'react-redux';
 import { Flex } from 'grapefruit-ui';
 import dynamic from 'next/dynamic';
 import { Button, Avatar } from '@primitives';
-import { fetchFullUserProfile, setIsEditing } from '@store';
+import { fetchFullUserProfile, setIsEditing, resetIsEditing } from '@store';
 import { NestedLayout } from '../common';
 import * as styles from './styles';
 import { ProfileData } from './primitives';
+import { useEffect } from 'react';
 
 const EditForm = dynamic(() => import('./edit-form'));
 const ChangeAvatar = dynamic(() => import('./change-avatar'));
@@ -15,13 +16,17 @@ const ChangeAvatar = dynamic(() => import('./change-avatar'));
 export const PersonalInfo = () => {
   const { t } = useTranslation();
   const { user: authUser } = useAuth();
+  const dispatch = useAppDispatch();
 
   const { user, loading, isEditing } = useTypedSelector(
     (state) => state.profile,
     shallowEqual,
   );
 
-  const dispatch = useAppDispatch();
+  useEffect(()=> {
+    dispatch(resetIsEditing());
+  },[dispatch]);
+
   const onGetFullProfile = () => dispatch(fetchFullUserProfile());
 
   const isAuthUser = authUser?.id === user?.id;
