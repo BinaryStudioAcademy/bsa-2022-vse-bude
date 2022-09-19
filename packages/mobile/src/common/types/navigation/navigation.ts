d506c0d7f5a1c5a05a89a4495d669d3f64e09d27
Oleh Mazur <69375560+mazurGit@users.noreply.github.com>
2022-09-19 14:36:43 +0300

    VBM-204: drawer trigger changed

--- packages/mobile/src/common/types/navigation/navigation.ts
+++ packages/mobile/src/common/types/navigation/navigation.ts
@@ -2,5 +2,7 @@ export type { RootNavigationParamList } from './root-navigation-param-list.type'
 export type {
   RootNavigationProps,
   MainNavigationProps,
+  DrawerNavigationProps,
 } from './navigation-props';
 export type { MainNavigationParamList } from './main-navigation-param-list';
+export type { DrawerNavigationParamList } from './drawer-navigation-param-list';
