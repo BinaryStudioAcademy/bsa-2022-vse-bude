1e3a88e75a3a17c2b2168e58fe2daa2e7fa9e707
Val_M <v.mischenko90@gmail.com>
2022-09-20 16:13:02 +0300

    change fields order

--- packages/mobile/src/screens/auth/components/sign-up-form/sign-up-form.tsx
+++ packages/mobile/src/screens/auth/components/sign-up-form/sign-up-form.tsx
@@ -24,13 +24,6 @@ const SignUpForm: React.FC<Props> = ({ onSubmit }) => {
   return (
     <View style={globalStyles.py5}>
       <Input
-        label={t('verification.EMAIL')}
-        placeholder={t('verification.EMAIL_HINT')}
-        name="email"
-        control={control}
-        errors={errors}
-      />
-      <Input
         label={t('verification.NAME')}
         placeholder={t('verification.NAME_HINT')}
         name="firstName"
@@ -45,6 +38,13 @@ const SignUpForm: React.FC<Props> = ({ onSubmit }) => {
         control={control}
         errors={errors}
         contentContainerStyle={globalStyles.mt5}
+      />
+      <Input
+        label={t('verification.EMAIL')}
+        placeholder={t('verification.EMAIL_HINT')}
+        name="email"
+        control={control}
+        errors={errors}
       />
       <Input
         label={t('verification.PHONE_NUMBER')}
