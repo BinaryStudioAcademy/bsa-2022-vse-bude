aab9e876b539feae26c5b76d4c8d696eaa72d4cc
dannleed <59970198+dannleed@users.noreply.github.com>
2022-09-20 23:56:40 +0300

    adjust filter page styles

---
+++ packages/frontend/components/filter/styles.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+export const container = css`
+  min-height: 675px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const productGridWrapper = css`
+  width: 100%;
+  height: 100%;
+`;
