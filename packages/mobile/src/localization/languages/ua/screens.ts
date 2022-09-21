92085f8ca85230982c7380af8dc5ff02c9aeef46
Val-M9 <71255723+Val-M9@users.noreply.github.com>
2022-09-21 10:04:24 +0300

    change buttons label (#468)

--- packages/mobile/src/localization/languages/ua/screens.ts
+++ packages/mobile/src/localization/languages/ua/screens.ts
@@ -227,7 +227,7 @@ export const screens = {
     TITLE: 'Створити публікацію',
     HEADER: 'Який тип продажу ви оберете?',
     AUCTION: 'Аукціон',
-    DIRECT_SALE: 'Продаж',
+    FIXED_PRICE: 'Фіксована ціна',
   },
   product_info: {
     TITLE: 'Деталі продукту',
