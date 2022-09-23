87ac74b514c4bb3e806da5b57f4e8ffe47ec0963
Aleksandr Termenzhy <aleksandr.termenzhy@binary-studio.com>
2022-09-23 19:10:06 +0300

    VBM: improved drawer layout

--- packages/mobile/src/navigation/drawer/components/drawer-content/drawer-content.tsx
+++ packages/mobile/src/navigation/drawer/components/drawer-content/drawer-content.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
 import { DrawerContentScrollView } from '@react-navigation/drawer';
+import { useTheme } from '@react-navigation/native';
+
+import { Logo, View } from '~/components/components';
 import { globalStyles } from '~/styles/styles';
-import { LanguageButtons } from '../language-buttons/language-buttons';
 import { DrawerMenu } from '../menu/drawer-menu';
+import { LanguageButtons } from '../language-buttons/language-buttons';
 
 const DrawerContent: FC = () => {
+  const { colors } = useTheme();
+
   return (
-    <DrawerContentScrollView style={globalStyles.mt4}>
+    <DrawerContentScrollView style={{ backgroundColor: colors.background }}>
+      <View style={[globalStyles.alignItemsCenter, globalStyles.py4]}>
+        <Logo width={150} height={50} />
+      </View>
       <LanguageButtons />
       <DrawerMenu />
     </DrawerContentScrollView>
--- packages/mobile/src/navigation/drawer/components/menu/styles.ts
+++ packages/mobile/src/navigation/drawer/components/menu/styles.ts
@@ -3,40 +3,15 @@ import { ColorPalette } from '@vse-bude/shared';
 import { FontSize } from '~/common/enums/enums';
 
 const styles = StyleSheet.create({
-  header: {
-    paddingVertical: 15,
-    fontSize: FontSize.TEXT_20,
-    fontWeight: '800',
-    textAlign: 'center',
-    borderBottomColor: ColorPalette.GRAY_200,
-    borderBottomWidth: 1,
-  },
-  title: {
-    marginLeft: -20,
-    marginRight: 6,
-    color: ColorPalette.GREEN_200,
-    fontWeight: '600',
-    FontSize: FontSize.TEXT_14,
-  },
-  subtitle: {
-    marginTop: 8,
-    marginBottom: 10,
-    marginLeft: 34,
-    FontSize: FontSize.TEXT_14,
-    color: ColorPalette.GREEN_200,
-    opacity: 0.6,
-  },
   label: {
-    marginLeft: 17,
     marginTop: -8,
     color: ColorPalette.GREEN_200,
     fontWeight: '400',
     FontSize: FontSize.TEXT_14,
   },
-  divider: {
-    borderColor: ColorPalette.GRAY_200,
-    marginRight: 20,
-    borderWidth: 1,
+  categoryItem: {
+    marginBottom: 0,
+    marginTop: 0,
   },
 });
 
